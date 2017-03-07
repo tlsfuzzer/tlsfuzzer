b690ad6a834579b1a9d022070a75f9ccd70716dd
Hubert Kario <hkario@redhat.com>
2017-03-07 12:19:25 +0100

    parametrise test-record-layer-fragmentation script
    allow for excluding single test cases, running single
    test cases and changing hostname and port

--- scripts/test-record-layer-fragmentation.py
+++ scripts/test-record-layer-fragmentation.py
@@ -4,6 +4,9 @@
 from __future__ import print_function
 import traceback
 import sys
+import getopt
+import re
+from itertools import chain
 
 from tlsfuzzer.runner import Runner
 from tlsfuzzer.messages import Connect, ClientHelloGenerator, \
@@ -18,14 +21,77 @@ from tlslite.constants import CipherSuite, AlertLevel, AlertDescription, \
         ExtensionType
 from tlslite.extensions import TLSExtension
 
-def main():
+def natural_sort_keys(s, _nsre=re.compile('([0-9]+)')):
+    return [int(text) if text.isdigit() else text.lower()
+            for text in re.split(_nsre, s)]
 
+
+def help_msg():
+    print("Usage: <script-name> [-h hostname] [-p port] [[probe-name] ...]")
+    print(" -h hostname    name of the host to run the test against")
+    print("                localhost by default")
+    print(" -p port        port number to use for connection, 4433 by default")
+    print(" probe-name     if present, will run only the probes with given")
+    print("                names and not all of them, e.g \"sanity\"")
+    print(" -e probe-name  exclude the probe from the list of the ones run")
+    print("                may be specified multiple times")
+    print(" --help         this message")
+
+def main():
     #
     # Test if server can handle handshake protocol messages fragmented over
     # multiple records
     #
 
+    host = "localhost"
+    port = 4433
+    run_exclude = set()
+
+    argv = sys.argv[1:]
+    opts, args = getopt.getopt(argv, "h:p:e:", ["help"])
+    for opt, arg in opts:
+        if opt == '-h':
+            host = arg
+        elif opt == '-p':
+            port = int(arg)
+        elif opt == '-e':
+            run_exclude.add(arg)
+        elif opt == '--help':
+            help_msg()
+            sys.exit(0)
+        else:
+            raise ValueError("Unknown option: {0}".format(opt))
+
+    if args:
+        run_only = set(args)
+    else:
+        run_only = None
+
     conversations = {}
+
+    conversation = Connect(host, port)
+    node = conversation
+    ciphers = [CipherSuite.TLS_RSA_WITH_AES_128_CBC_SHA,
+               CipherSuite.TLS_EMPTY_RENEGOTIATION_INFO_SCSV]
+    ext = {21: TLSExtension().create(21, bytearray(10))}
+    node = node.add_child(ClientHelloGenerator(ciphers,
+                                               extensions=ext))
+    node = node.add_child(ExpectServerHello(extensions={ExtensionType.renegotiation_info:None}))
+    node = node.add_child(ExpectCertificate())
+    node = node.add_child(ExpectServerHelloDone())
+    node = node.add_child(ClientKeyExchangeGenerator())
+    node = node.add_child(ChangeCipherSpecGenerator())
+    node = node.add_child(FinishedGenerator())
+    node = node.add_child(ExpectChangeCipherSpec())
+    node = node.add_child(ExpectFinished())
+    node = node.add_child(ApplicationDataGenerator(bytearray(b"GET / HTTP/1.0\n\n")))
+    node = node.add_child(ExpectApplicationData())
+    node = node.add_child(AlertGenerator(AlertLevel.warning,
+                                         AlertDescription.close_notify))
+    node = node.add_child(ExpectAlert())
+    node.next_sibling = ExpectClose()
+
+    conversations["sanity"] = conversation
 
     # 2**14-49 - max size of Client Hello for OpenSSL
     # 2**16-5 - max size of extensions in TLS
@@ -53,7 +119,7 @@ def main():
                                 ("medium, maximum fragmentation", 1024, 1),
                                 ("maximum size without fragmentation", 2**14-53, None)]:
 
-        conversation = Connect("localhost", 4433)
+        conversation = Connect(host, port)
         node = conversation
         node = node.add_child(SetMaxRecordSize(record_len))
         ciphers = [CipherSuite.TLS_RSA_WITH_AES_128_CBC_SHA,
@@ -86,7 +152,7 @@ def main():
     # check if records bigger than TLSPlaintext limit are rejected
     padding_extension = TLSExtension().create(21, bytearray(2**14-52))
 
-    conversation = Connect("localhost", 4433)
+    conversation = Connect(host, port)
     node = conversation
     node = node.add_child(SetMaxRecordSize(2**16-1))
     ciphers = [CipherSuite.TLS_RSA_WITH_AES_128_CBC_SHA,
@@ -102,30 +168,43 @@ def main():
     # run the conversation
     good = 0
     bad = 0
+    failed = []
 
-    for conversation_name in conversations:
-        conversation = conversations[conversation_name]
+    # make sure that sanity test is run first and last
+    # to verify that server was running and kept running throught
+    sanity_test = ('sanity', conversations['sanity'])
+    ordered_tests = chain([sanity_test],
+                          filter(lambda x: x[0] != 'sanity',
+                                 conversations.items()),
+                          [sanity_test])
 
-        runner = Runner(conversation)
+    for c_name, c_test in ordered_tests:
+        if run_only and c_name not in run_only or c_name in run_exclude:
+            continue
+        print("{0} ...".format(c_name))
 
-        print(str(conversation_name) + "...\n")
+        runner = Runner(c_test)
+
         res = True
         try:
             runner.run()
-            print("OK")
         except:
             print("Error while processing")
             print(traceback.format_exc())
             res = False
 
         if res:
-            good+=1
+            good += 1
+            print("OK\n")
         else:
-            bad+=1
+            bad += 1
+            failed.append(c_name)
 
     print("Test end")
     print("successful: {0}".format(good))
     print("failed: {0}".format(bad))
+    failed_sorted = sorted(failed, key=natural_sort_keys)
+    print("  {0}".format('\n  '.join(repr(i) for i in failed_sorted)))
 
     if bad > 0:
         sys.exit(1)
