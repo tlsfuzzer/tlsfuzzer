b36eea56dc9a251d1d7ff5d4e9d1c70593920d46
Hubert Kario <hkario@redhat.com>
2017-04-04 18:47:30 +0200

    EMS with client certificates

---
+++ scripts/test-extended-master-secret-extension-with-client-cert.py
@@ -0,0 +1,219 @@
+# Author: Hubert Kario, (c) 2016
+# Released under Gnu GPL v2.0, see LICENSE file for details
+"""Test EMS with CertificateRequest"""
+
+from __future__ import print_function
+import traceback
+import sys
+import getopt
+import re
+from itertools import chain
+
+from tlsfuzzer.runner import Runner
+from tlsfuzzer.messages import Connect, ClientHelloGenerator, \
+        ClientKeyExchangeGenerator, ChangeCipherSpecGenerator, \
+        FinishedGenerator, ApplicationDataGenerator, \
+        CertificateGenerator, CertificateVerifyGenerator, \
+        AlertGenerator
+from tlsfuzzer.expect import ExpectServerHello, ExpectCertificate, \
+        ExpectServerHelloDone, ExpectChangeCipherSpec, ExpectFinished, \
+        ExpectAlert, ExpectClose, ExpectCertificateRequest, \
+        ExpectApplicationData
+from tlslite.extensions import SignatureAlgorithmsExtension
+from tlslite.constants import CipherSuite, AlertDescription, \
+        HashAlgorithm, SignatureAlgorithm, ExtensionType
+from tlslite.utils.keyfactory import parsePEMKey
+from tlslite.x509 import X509
+from tlslite.x509certchain import X509CertChain
+
+
+def natural_sort_keys(s, _nsre=re.compile('([0-9]+)')):
+    return [int(text) if text.isdigit() else text.lower()
+            for text in re.split(_nsre, s)]
+
+
+def help_msg():
+    print("Usage: <script-name> [-h hostname] [-p port] [[probe-name] ...]")
+    print(" -h hostname    name of the host to run the test against")
+    print("                localhost by default")
+    print(" -p port        port number to use for connection, 4433 by default")
+    print(" probe-name     if present, will run only the probes with given")
+    print("                names and not all of them, e.g \"sanity\"")
+    print(" -e probe-name  exclude the probe from the list of the ones run")
+    print("                may be specified multiple times")
+    print(" -s sigalgs     hash and signature algorithm pairs that the server")
+    print("                is expected to support. \"sha512+rsa sha384+rsa ")
+    print("                sha256+rsa sha224+rsa sha1+rsa\" by default")
+    print(" -k keyfile     file with private key of client")
+    print(" -c certfile    file with the certificate of client")
+    print(" --help         this message")
+
+
+def main():
+    """Check if EMS with client certificates is supported"""
+    hostname = "localhost"
+    port = 4433
+    run_exclude = set()
+    sigalgs = [(HashAlgorithm.sha512, SignatureAlgorithm.rsa),
+               (HashAlgorithm.sha384, SignatureAlgorithm.rsa),
+               (HashAlgorithm.sha256, SignatureAlgorithm.rsa),
+               (HashAlgorithm.sha224, SignatureAlgorithm.rsa),
+               (HashAlgorithm.sha1, SignatureAlgorithm.rsa)]
+    cert = None
+    private_key = None
+
+    argv = sys.argv[1:]
+    opts, args = getopt.getopt(argv, "h:p:e:s:k:c:", ["help"])
+    for opt, arg in opts:
+        if opt == '-h':
+            host = arg
+        elif opt == '-p':
+            port = int(arg)
+        elif opt == '-e':
+            run_exclude.add(arg)
+        elif opt == '--help':
+            help_msg()
+            sys.exit(0)
+        elif opt == '-s':
+            sigalgs = [(getattr(HashAlgorithm, h_alg),
+                        getattr(SignatureAlgorithm, s_alg)) for h_alg, s_alg
+                        in (alg.split('+') for alg in arg.split(' '))]
+        elif opt == '-k':
+            text_key = open(arg, 'rb').read()
+            if sys.version_info[0] >= 3:
+                text_key = str(text_key, 'utf-8')
+            private_key = parsePEMKey(text_key, private=True)
+        elif opt == '-c':
+            text_cert = open(arg, 'rb').read()
+            if sys.version_info[0] >= 3:
+                text_cert = str(text_cert, 'utf-8')
+            cert = X509()
+            cert.parse(text_cert)
+        else:
+            raise ValueError("Unknown option: {0}".format(opt))
+
+    if args:
+        run_only = set(args)
+    else:
+        run_only = None
+
+    conversations = {}
+
+    # sanity check for Client Certificates
+    conversation = Connect(hostname, port)
+    node = conversation
+    ciphers = [CipherSuite.TLS_RSA_WITH_AES_128_CBC_SHA,
+               CipherSuite.TLS_EMPTY_RENEGOTIATION_INFO_SCSV]
+    ext = {ExtensionType.signature_algorithms :
+           SignatureAlgorithmsExtension().create([
+             (getattr(HashAlgorithm, x),
+              SignatureAlgorithm.rsa) for x in ['sha512', 'sha384', 'sha256',
+                                                'sha224', 'sha1', 'md5']])}
+    ext[ExtensionType.extended_master_secret] = None
+    node = node.add_child(ClientHelloGenerator(ciphers, extensions=ext))
+    ext = {ExtensionType.renegotiation_info:None,
+           ExtensionType.extended_master_secret:None}
+    node = node.add_child(ExpectServerHello(version=(3, 3), extensions=ext))
+    node = node.add_child(ExpectCertificate())
+    node = node.add_child(ExpectCertificateRequest())
+    node = node.add_child(ExpectServerHelloDone())
+    node = node.add_child(CertificateGenerator())
+    node = node.add_child(ClientKeyExchangeGenerator())
+    node = node.add_child(ChangeCipherSpecGenerator())
+    node = node.add_child(FinishedGenerator())
+    node = node.add_child(ExpectChangeCipherSpec())
+    node = node.add_child(ExpectFinished())
+    node = node.add_child(ApplicationDataGenerator(b"GET / HTTP/1.0\n\n"))
+    node = node.add_child(ExpectApplicationData())
+    node = node.add_child(AlertGenerator(AlertDescription.close_notify))
+    node = node.add_child(ExpectClose())
+    node.next_sibling = ExpectAlert()
+    node.next_sibling.add_child(ExpectClose())
+
+    conversations["sanity"] = conversation
+
+    if cert and private_key:
+        # sanity check for Client Certificates
+        conversation = Connect(hostname, port)
+        node = conversation
+        ciphers = [CipherSuite.TLS_RSA_WITH_AES_128_CBC_SHA,
+                   CipherSuite.TLS_EMPTY_RENEGOTIATION_INFO_SCSV]
+        ext = {ExtensionType.signature_algorithms :
+               SignatureAlgorithmsExtension().create([
+                 (getattr(HashAlgorithm, x),
+                  SignatureAlgorithm.rsa) for x in ['sha512', 'sha384', 'sha256',
+                                                    'sha224', 'sha1', 'md5']])}
+        ext[ExtensionType.extended_master_secret] = None
+        node = node.add_child(ClientHelloGenerator(ciphers, extensions=ext))
+        ext = {ExtensionType.renegotiation_info:None,
+               ExtensionType.extended_master_secret:None}
+        node = node.add_child(ExpectServerHello(version=(3, 3), extensions=ext))
+        node = node.add_child(ExpectCertificate())
+        node = node.add_child(ExpectCertificateRequest())
+        node = node.add_child(ExpectServerHelloDone())
+        node = node.add_child(CertificateGenerator(X509CertChain([cert])))
+        node = node.add_child(ClientKeyExchangeGenerator())
+        node = node.add_child(CertificateVerifyGenerator(private_key))
+        node = node.add_child(ChangeCipherSpecGenerator())
+        node = node.add_child(FinishedGenerator())
+        node = node.add_child(ExpectChangeCipherSpec())
+        node = node.add_child(ExpectFinished())
+        node = node.add_child(ApplicationDataGenerator(b"GET / HTTP/1.0\n\n"))
+        node = node.add_child(ExpectApplicationData())
+        node = node.add_child(AlertGenerator(AlertDescription.close_notify))
+        node = node.add_child(ExpectClose())
+        node.next_sibling = ExpectAlert()
+        node.next_sibling.add_child(ExpectClose())
+
+        conversations["with certificate"] = conversation
+
+    # run the conversation
+    good = 0
+    bad = 0
+    failed = []
+
+    # make sure that sanity test is run first and last
+    # to verify that server was running and kept running throught
+    sanity_test = ('sanity', conversations['sanity'])
+    ordered_tests = chain([sanity_test],
+                          filter(lambda x: x[0] != 'sanity',
+                                 conversations.items()),
+                          [sanity_test])
+
+    for c_name, c_test in ordered_tests:
+        if run_only and c_name not in run_only or c_name in run_exclude:
+            continue
+        print("{0} ...".format(c_name))
+
+        runner = Runner(c_test)
+
+        res = True
+        try:
+            runner.run()
+        except:
+            print("Error while processing")
+            print(traceback.format_exc())
+            res = False
+
+        if res:
+            good += 1
+            print("OK\n")
+        else:
+            bad += 1
+            failed.append(c_name)
+
+    print("Test to verify if server supports extended master secret with ")
+    print("client certificates.\n")
+    print("Test version 1\n")
+
+    print("Test end")
+    print("successful: {0}".format(good))
+    print("failed: {0}".format(bad))
+    failed_sorted = sorted(failed, key=natural_sort_keys)
+    print("  {0}".format('\n  '.join(repr(i) for i in failed_sorted)))
+
+    if bad > 0:
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
