1f545161fdec272cc3028bc73b2eeec13caee7f0
Hubert Kario <hkario@redhat.com>
2017-05-05 12:48:26 +0200

    tests for checking correctness of SNI hostnames

---
+++ scripts/test-invalid-server-name-extension.py
@@ -0,0 +1,416 @@
+# Author: Hubert Kario, (c) 2016
+# Released under Gnu GPL v2.0, see LICENSE file for details
+"""Tests for SNI"""
+from __future__ import print_function
+import traceback
+import sys
+import getopt
+import re
+from itertools import chain
+
+from tlsfuzzer.runner import Runner
+from tlsfuzzer.messages import Connect, ClientHelloGenerator, \
+        ClientKeyExchangeGenerator, ChangeCipherSpecGenerator, \
+        FinishedGenerator, ApplicationDataGenerator, AlertGenerator
+from tlsfuzzer.expect import ExpectServerHello, ExpectCertificate, \
+        ExpectServerHelloDone, ExpectChangeCipherSpec, ExpectFinished, \
+        ExpectAlert, ExpectClose, ExpectApplicationData
+
+from tlslite.constants import CipherSuite, AlertLevel, AlertDescription, \
+        ExtensionType, HashAlgorithm, SignatureAlgorithm, NameType
+from tlslite.extensions import TLSExtension, SignatureAlgorithmsExtension, \
+        SNIExtension
+
+
+def natural_sort_keys(s, _nsre=re.compile('([0-9]+)')):
+    return [int(text) if text.isdigit() else text.lower()
+            for text in re.split(_nsre, s)]
+
+
+def help_msg():
+    print("Usage: <script-name> [-h hostname] [-p port] [[probe-name] ...]")
+    print(" -h hostname    name of the host to run the test against")
+    print("                localhost by default")
+    print(" -p port        port number to use for connection, 4433 by default")
+    print(" probe-name     if present, will run only the probes with given")
+    print("                names and not all of them, e.g \"sanity\"")
+    print(" -e probe-name  exclude the probe from the list of the ones run")
+    print("                may be specified multiple times")
+    print(" --sni hostname name the server expects to recieve with SNI")
+    print("                \"localhost\" by default")
+    print(" --sni-fatal    expect unrecognised names to be fatal alerts")
+    print(" --help         this message")
+
+
+def main():
+    """check if server handles malformed server name indication extension"""
+    host = "localhost"
+    hostname = "localhost"
+    port = 4433
+    run_exclude = set()
+    sni_fatal = False
+
+    argv = sys.argv[1:]
+    opts, args = getopt.getopt(argv, "h:p:e:", ["help", "sni=", "sni-fatal"])
+    for opt, arg in opts:
+        if opt == '-h':
+            host = arg
+        elif opt == '-p':
+            port = int(arg)
+        elif opt == '-e':
+            run_exclude.add(arg)
+        elif opt == '--help':
+            help_msg()
+            sys.exit(0)
+        elif opt == '--sni':
+            hostname = arg
+        elif opt == '--sni-fatal':
+            sni_fatal = True
+        else:
+            raise ValueError("Unknown option: {0}".format(opt))
+
+    if args:
+        run_only = set(args)
+    else:
+        run_only = None
+
+    conversations = {}
+
+    # sanity check without SNI
+    conversation = Connect(host, port)
+    node = conversation
+    ciphers = [CipherSuite.TLS_RSA_WITH_AES_128_CBC_SHA,
+               CipherSuite.TLS_EMPTY_RENEGOTIATION_INFO_SCSV]
+    ext = {ExtensionType.signature_algorithms :
+           SignatureAlgorithmsExtension().create([
+             (getattr(HashAlgorithm, x),
+              SignatureAlgorithm.rsa) for x in ['sha512', 'sha384', 'sha256',
+                                                'sha224', 'sha1']])}
+    node = node.add_child(ClientHelloGenerator(ciphers, extensions=ext))
+    node = node.add_child(ExpectServerHello(version=(3, 3)))
+    node = node.add_child(ExpectCertificate())
+    node = node.add_child(ExpectServerHelloDone())
+    node = node.add_child(ClientKeyExchangeGenerator())
+    node = node.add_child(ChangeCipherSpecGenerator())
+    node = node.add_child(FinishedGenerator())
+    node = node.add_child(ExpectChangeCipherSpec())
+    node = node.add_child(ExpectFinished())
+    node = node.add_child(ApplicationDataGenerator(b"GET / HTTP/1.0\n\n"))
+    node = node.add_child(ExpectApplicationData())
+    node = node.add_child(AlertGenerator(AlertDescription.close_notify))
+    node = node.add_child(ExpectClose())
+    node.next_sibling = ExpectAlert(AlertLevel.warning,
+                                    AlertDescription.close_notify)
+    node.next_sibling.add_child(ExpectClose())
+    conversations["sanity"] = conversation
+
+    # sanity check SNI
+    conversation = Connect(host, port)
+    node = conversation
+    ciphers = [CipherSuite.TLS_RSA_WITH_AES_128_CBC_SHA,
+               CipherSuite.TLS_EMPTY_RENEGOTIATION_INFO_SCSV]
+    ext = {ExtensionType.signature_algorithms :
+           SignatureAlgorithmsExtension().create([
+             (getattr(HashAlgorithm, x),
+              SignatureAlgorithm.rsa) for x in ['sha512', 'sha384', 'sha256',
+                                                'sha224', 'sha1']])}
+    sni = SNIExtension().create(bytearray(hostname, 'utf-8'))
+    ext[ExtensionType.server_name] = sni
+    node = node.add_child(ClientHelloGenerator(ciphers, extensions=ext))
+    node = node.add_child(ExpectServerHello(version=(3, 3)))
+    node = node.add_child(ExpectCertificate())
+    node = node.add_child(ExpectServerHelloDone())
+    node = node.add_child(ClientKeyExchangeGenerator())
+    node = node.add_child(ChangeCipherSpecGenerator())
+    node = node.add_child(FinishedGenerator())
+    node = node.add_child(ExpectChangeCipherSpec())
+    node = node.add_child(ExpectFinished())
+    node = node.add_child(ApplicationDataGenerator(b"GET / HTTP/1.0\n\n"))
+    node = node.add_child(ExpectApplicationData())
+    node = node.add_child(AlertGenerator(AlertDescription.close_notify))
+    node = node.add_child(ExpectClose())
+    node.next_sibling = ExpectAlert(AlertLevel.warning,
+                                    AlertDescription.close_notify)
+    node.next_sibling.add_child(ExpectClose())
+    conversations["Sanity check, SNI"] = conversation
+
+    # incorrect host name
+    conversation = Connect(host, port)
+    node = conversation
+    ciphers = [CipherSuite.TLS_RSA_WITH_AES_128_CBC_SHA,
+               CipherSuite.TLS_EMPTY_RENEGOTIATION_INFO_SCSV]
+    ext = {ExtensionType.signature_algorithms :
+           SignatureAlgorithmsExtension().create([
+             (getattr(HashAlgorithm, x),
+              SignatureAlgorithm.rsa) for x in ['sha512', 'sha384', 'sha256',
+                                                'sha224', 'sha1']])}
+    sni = SNIExtension().create(bytearray(b'www.') +
+                                bytearray(hostname, 'utf-8'))
+    ext[ExtensionType.server_name] = sni
+    node = node.add_child(ClientHelloGenerator(ciphers, extensions=ext))
+    if sni_fatal:
+        node = node.add_child(ExpectAlert(AlertLevel.fatal,
+                                          AlertDescription.unrecognized_name))
+        node = node.add_child(ExpectClose())
+    else:
+        node = node.add_child(ExpectAlert(AlertLevel.warning,
+                                          AlertDescription.unrecognized_name))
+        node = node.add_child(ExpectServerHello(version=(3, 3)))
+        node = node.add_child(ExpectCertificate())
+        node = node.add_child(ExpectServerHelloDone())
+        node = node.add_child(ClientKeyExchangeGenerator())
+        node = node.add_child(ChangeCipherSpecGenerator())
+        node = node.add_child(FinishedGenerator())
+        node = node.add_child(ExpectChangeCipherSpec())
+        node = node.add_child(ExpectFinished())
+        node = node.add_child(ApplicationDataGenerator(b"GET / HTTP/1.0\n\n"))
+        node = node.add_child(ExpectApplicationData())
+        node = node.add_child(AlertGenerator(AlertDescription.close_notify))
+        node = node.add_child(ExpectClose())
+        node.next_sibling = ExpectAlert(AlertLevel.warning,
+                                        AlertDescription.close_notify)
+        node.next_sibling.add_child(ExpectClose())
+    conversations["incorrect SNI"] = conversation
+
+    # SNI name with NULL
+    conversation = Connect(host, port)
+    node = conversation
+    ciphers = [CipherSuite.TLS_RSA_WITH_AES_128_CBC_SHA,
+               CipherSuite.TLS_EMPTY_RENEGOTIATION_INFO_SCSV]
+    ext = {ExtensionType.signature_algorithms :
+           SignatureAlgorithmsExtension().create([
+             (getattr(HashAlgorithm, x),
+              SignatureAlgorithm.rsa) for x in ['sha512', 'sha384', 'sha256',
+                                                'sha224', 'sha1']])}
+    # names MUST be valid DNS host names
+    sni = SNIExtension().create(bytearray(hostname[:-1], 'utf-8') +
+                                bytearray(b'\x00') +
+                                bytearray(hostname[-1:], 'utf-8'))
+    ext[ExtensionType.server_name] = sni
+    node = node.add_child(ClientHelloGenerator(ciphers, extensions=ext))
+    node = node.add_child(ExpectAlert(AlertLevel.fatal,
+                                      AlertDescription.illegal_parameter))
+    node = node.add_child(ExpectClose())
+
+    conversations["SNI name with NULL"] = conversation
+
+    # SNI name with special character
+    conversation = Connect(host, port)
+    node = conversation
+    ciphers = [CipherSuite.TLS_RSA_WITH_AES_128_CBC_SHA,
+               CipherSuite.TLS_EMPTY_RENEGOTIATION_INFO_SCSV]
+    ext = {ExtensionType.signature_algorithms :
+           SignatureAlgorithmsExtension().create([
+             (getattr(HashAlgorithm, x),
+              SignatureAlgorithm.rsa) for x in ['sha512', 'sha384', 'sha256',
+                                                'sha224', 'sha1']])}
+    # names MUST be valid DNS host names
+    sni = SNIExtension().create(bytearray(hostname[:-1], 'utf-8') +
+                                bytearray(b'\x07') +
+                                bytearray(hostname[-1:], 'utf-8'))
+    ext[ExtensionType.server_name] = sni
+    node = node.add_child(ClientHelloGenerator(ciphers, extensions=ext))
+    node = node.add_child(ExpectAlert(AlertLevel.fatal,
+                                      AlertDescription.illegal_parameter))
+    node = node.add_child(ExpectClose())
+
+    conversations["SNI name with BEL"] = conversation
+
+    # SNI name with UTF-8 character
+    conversation = Connect(host, port)
+    node = conversation
+    ciphers = [CipherSuite.TLS_RSA_WITH_AES_128_CBC_SHA,
+               CipherSuite.TLS_EMPTY_RENEGOTIATION_INFO_SCSV]
+    ext = {ExtensionType.signature_algorithms :
+           SignatureAlgorithmsExtension().create([
+             (getattr(HashAlgorithm, x),
+              SignatureAlgorithm.rsa) for x in ['sha512', 'sha384', 'sha256',
+                                                'sha224', 'sha1']])}
+    # names MUST be valid DNS host names
+    sni = SNIExtension().create(bytearray(hostname[:-1], 'utf-8') +
+                                bytearray(b'\xc4\x85') +
+                                bytearray(hostname[-1:], 'utf-8'))
+    ext[ExtensionType.server_name] = sni
+    node = node.add_child(ClientHelloGenerator(ciphers, extensions=ext))
+    node = node.add_child(ExpectAlert(AlertLevel.fatal,
+                                      AlertDescription.illegal_parameter))
+    node = node.add_child(ExpectClose())
+
+    conversations["SNI name with UTF-8"] = conversation
+
+    conversation = Connect(host, port)
+    node = conversation
+    ciphers = [CipherSuite.TLS_RSA_WITH_AES_128_CBC_SHA,
+               CipherSuite.TLS_EMPTY_RENEGOTIATION_INFO_SCSV]
+    ext = {ExtensionType.signature_algorithms :
+           SignatureAlgorithmsExtension().create([
+             (getattr(HashAlgorithm, x),
+              SignatureAlgorithm.rsa) for x in ['sha512', 'sha384', 'sha256',
+                                                'sha224', 'sha1']])}
+    # names MUST be valid DNS host names
+    sni = SNIExtension().create(bytearray(hostname, 'utf-8') +
+                                bytearray(b'\x1b[31mBAD\x1b[0;37m'))
+    ext[ExtensionType.server_name] = sni
+    node = node.add_child(ClientHelloGenerator(ciphers, extensions=ext))
+    node = node.add_child(ExpectAlert(AlertLevel.fatal,
+                                      AlertDescription.illegal_parameter))
+    node = node.add_child(ExpectClose())
+
+    conversations["SNI name with ANSI color escapes code"] = conversation
+
+
+    # malformed extension
+    conversation = Connect(host, port)
+    node = conversation
+    ciphers = [CipherSuite.TLS_RSA_WITH_AES_128_CBC_SHA]
+    ext = {ExtensionType.server_name: lambda _:TLSExtension().create(0, bytearray(b'\xff'*4))}
+    node = node.add_child(ClientHelloGenerator(ciphers,
+                                               extensions=ext))
+    node = node.add_child(ExpectAlert(AlertLevel.fatal,
+                                      AlertDescription.decode_error))
+    node = node.add_child(ExpectClose())
+    conversations["malformed overall length"] = conversation
+
+    # multiple names in SNI
+    conversation = Connect(host, port)
+    node = conversation
+    ciphers = [CipherSuite.TLS_RSA_WITH_AES_128_CBC_SHA,
+               CipherSuite.TLS_EMPTY_RENEGOTIATION_INFO_SCSV]
+    ext = {ExtensionType.signature_algorithms :
+           SignatureAlgorithmsExtension().create([
+             (getattr(HashAlgorithm, x),
+              SignatureAlgorithm.rsa) for x in ['sha512', 'sha384', 'sha256',
+                                                'sha224', 'sha1']])}
+    # RFC 6066 client MUST NOT send two names of the same type
+    sni = SNIExtension().create(hostNames=[bytearray(hostname, 'utf-8'),
+                                           bytearray(b'www.') +
+                                           bytearray(hostname, 'utf-8')])
+    ext[ExtensionType.server_name] = sni
+    node = node.add_child(ClientHelloGenerator(ciphers, extensions=ext))
+    node = node.add_child(ExpectAlert(AlertLevel.fatal,
+                                      AlertDescription.illegal_parameter))
+    node = node.add_child(ExpectClose())
+    conversations["multiple host_names in SNI, RFC 6066 compliance"] = conversation
+
+    # multiple types in SNI
+    conversation = Connect(host, port)
+    node = conversation
+    ciphers = [CipherSuite.TLS_RSA_WITH_AES_128_CBC_SHA,
+               CipherSuite.TLS_EMPTY_RENEGOTIATION_INFO_SCSV]
+    ext = {ExtensionType.signature_algorithms :
+           SignatureAlgorithmsExtension().create([
+             (getattr(HashAlgorithm, x),
+              SignatureAlgorithm.rsa) for x in ['sha512', 'sha384', 'sha256',
+                                                'sha224', 'sha1']])}
+    names = [SNIExtension.ServerName(NameType.host_name,
+                                     bytearray(hostname, 'utf-8')),
+             # some unknown SNI type, should be ignored by server
+             SNIExtension.ServerName(NameType.host_name + 1,
+                                     bytearray(range(0, 24)))]
+    sni = SNIExtension().create(serverNames=names)
+    ext[ExtensionType.server_name] = sni
+    node = node.add_child(ClientHelloGenerator(ciphers, extensions=ext))
+    node = node.add_child(ExpectServerHello(version=(3, 3)))
+    node = node.add_child(ExpectCertificate())
+    node = node.add_child(ExpectServerHelloDone())
+    node = node.add_child(ClientKeyExchangeGenerator())
+    node = node.add_child(ChangeCipherSpecGenerator())
+    node = node.add_child(FinishedGenerator())
+    node = node.add_child(ExpectChangeCipherSpec())
+    node = node.add_child(ExpectFinished())
+    node = node.add_child(ApplicationDataGenerator(b"GET / HTTP/1.0\n\n"))
+    node = node.add_child(ExpectApplicationData())
+    node = node.add_child(AlertGenerator(AlertDescription.close_notify))
+    node = node.add_child(ExpectClose())
+    node.next_sibling = ExpectAlert(AlertLevel.warning,
+                                    AlertDescription.close_notify)
+    node.next_sibling.add_child(ExpectClose())
+    conversations["multiple types in SNI, host_name first"] = conversation
+
+    # multiple types in SNI, host_name last
+    conversation = Connect(host, port)
+    node = conversation
+    ciphers = [CipherSuite.TLS_RSA_WITH_AES_128_CBC_SHA,
+               CipherSuite.TLS_EMPTY_RENEGOTIATION_INFO_SCSV]
+    ext = {ExtensionType.signature_algorithms :
+           SignatureAlgorithmsExtension().create([
+             (getattr(HashAlgorithm, x),
+              SignatureAlgorithm.rsa) for x in ['sha512', 'sha384', 'sha256',
+                                                'sha224', 'sha1']])}
+    names = [# some unknown SNI type, should be ignored by server
+             SNIExtension.ServerName(NameType.host_name + 1,
+                                     bytearray(range(0, 24))),
+             # actual SNI payload
+             SNIExtension.ServerName(NameType.host_name,
+                                     bytearray(hostname, 'utf-8'))]
+    sni = SNIExtension().create(serverNames=names)
+    ext[ExtensionType.server_name] = sni
+    node = node.add_child(ClientHelloGenerator(ciphers, extensions=ext))
+    node = node.add_child(ExpectServerHello(version=(3, 3)))
+    node = node.add_child(ExpectCertificate())
+    node = node.add_child(ExpectServerHelloDone())
+    node = node.add_child(ClientKeyExchangeGenerator())
+    node = node.add_child(ChangeCipherSpecGenerator())
+    node = node.add_child(FinishedGenerator())
+    node = node.add_child(ExpectChangeCipherSpec())
+    node = node.add_child(ExpectFinished())
+    node = node.add_child(ApplicationDataGenerator(b"GET / HTTP/1.0\n\n"))
+    node = node.add_child(ExpectApplicationData())
+    node = node.add_child(AlertGenerator(AlertDescription.close_notify))
+    node = node.add_child(ExpectClose())
+    node.next_sibling = ExpectAlert(AlertLevel.warning,
+                                    AlertDescription.close_notify)
+    node.next_sibling.add_child(ExpectClose())
+    # hangs gnutls-serv
+    conversations["multiple types in SNI, host_name last"] = conversation
+
+
+    # run the conversation
+    good = 0
+    bad = 0
+    failed = []
+
+    # make sure that sanity test is run first and last
+    # to verify that server was running and kept running throught
+    sanity_test = ('sanity', conversations['sanity'])
+    ordered_tests = chain([sanity_test],
+                          filter(lambda x: x[0] != 'sanity',
+                                 conversations.items()),
+                          [sanity_test])
+
+    for c_name, c_test in ordered_tests:
+        if run_only and c_name not in run_only or c_name in run_exclude:
+            continue
+        print("{0} ...".format(c_name))
+
+        runner = Runner(c_test)
+
+        res = True
+        try:
+            runner.run()
+        except:
+            print("Error while processing")
+            print(traceback.format_exc())
+            res = False
+
+        if res:
+            good += 1
+            print("OK\n")
+        else:
+            bad += 1
+            failed.append(c_name)
+
+    print("SNI extension test version 3")
+
+    print("Test end")
+    print("successful: {0}".format(good))
+    print("failed: {0}".format(bad))
+    failed_sorted = sorted(failed, key=natural_sort_keys)
+    print("  {0}".format('\n  '.join(repr(i) for i in failed_sorted)))
+
+    if bad > 0:
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
