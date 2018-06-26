4ec0e9e097f95cd9a92098b25a30584c84602cd0
Gekios <ggekios@gmail.com>
2018-06-26 20:38:35 +0200

    added missing tests in .yml and Makefile and fixed indentations in the script

--- tests/verify-scripts-json.py
+++ tests/verify-scripts-json.py
@@ -7,33 +7,31 @@ from os.path import isfile, join, dirname, abspath
 from sys import argv
 
 parent_dir = dirname(dirname(abspath(__file__)))
-print(parent_dir)
 scriptlist = [f for f in listdir(os.path.join(parent_dir,'scripts')) if isfile(join(os.path.join(parent_dir,'scripts'), f))]
 missing = []
 status = 0
 for arg in argv[1:]:
-	try:
-		jsonfile = json.load(open(arg))
-	except IOError:
-		print("Please check the input file name, it doesn't appear to exist")
-		status =1
-		continue
-	except ValueError:
-		print("Wrong file type: The input must be a json file")
-		status =1
-		continue
-	except IndexError:
-		print("No input file was provided")
-		status =1
-		continue
-
-	for f in scriptlist:
-		if f not in str(jsonfile):
-			missing.append(f)
-	if not missing:
-		print(" All scripts are in the json file")		
-	else:
-		print("There are " + str(len(missing)) + " scripts that are missing from " +str(arg)+":")
-		print("\n".join(missing))
-		status = 1
+    try:
+        jsonfile = json.load(open(arg))
+    except IOError:
+        print("Please check the input file name, it doesn't appear to exist")
+        status =1
+        continue
+    except ValueError:
+        print("Wrong file type: The input must be a json file")
+        status =1
+        continue
+    except IndexError:
+        print("No input file was provided")
+        status =1
+        continue
+    for f in scriptlist:
+        if f not in str(jsonfile):
+            missing.append(f)
+    if not missing:
+        print(" All scripts are in the json file")
+    else:
+        print("There are " + str(len(missing)) + " scripts that are missing from " +str(arg)+":")
+        print("\n".join(missing))
+        status = 1
 exit(status)
