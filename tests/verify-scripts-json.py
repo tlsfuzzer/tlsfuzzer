42b2f56b5e6c850c98b28da4a086b5226a91edb5
Gekios <ggekios@gmail.com>
2018-06-26 19:24:08 +0200

    Fixed issues based on the feedback

--- tests/verify-scripts-json.py
+++ tests/verify-scripts-json.py
@@ -6,31 +6,34 @@ import json
 from os.path import isfile, join, dirname, abspath
 from sys import argv
 
-dir = dirname(dirname(abspath(__file__)))
-print(dir)
-scriptlist = [f for f in listdir(os.path.join(dir,'scripts')) if isfile(join(os.path.join(dir,'scripts'), f))]
+parent_dir = dirname(dirname(abspath(__file__)))
+print(parent_dir)
+scriptlist = [f for f in listdir(os.path.join(parent_dir,'scripts')) if isfile(join(os.path.join(parent_dir,'scripts'), f))]
 missing = []
+status = 0
+for arg in argv[1:]:
+	try:
+		jsonfile = json.load(open(arg))
+	except IOError:
+		print("Please check the input file name, it doesn't appear to exist")
+		status =1
+		continue
+	except ValueError:
+		print("Wrong file type: The input must be a json file")
+		status =1
+		continue
+	except IndexError:
+		print("No input file was provided")
+		status =1
+		continue
 
-try:
-	jsonfile = json.load(open(argv[1]))
-except IOError:
-	print("Please check the input file name, it doesn't appear to exist")
-	exit(1)
-except ValueError:
-	print("Wrong file type: The input must be a json file")
-	exit(1)
-except IndexError:
-	print("No input file was provided")
-	exit(1)
-
-for f in scriptlist:
-	if f not in str(jsonfile):
-		missing.append(f)
-
-if not missing:
-	print(" All scripts are in the json file")
-	exit(0)
-else:
-	print("There are " + str(len(missing)) + " scripts that are missing from the file:")
-	print("\n".join(missing))
-	exit(1)
+	for f in scriptlist:
+		if f not in str(jsonfile):
+			missing.append(f)
+	if not missing:
+		print(" All scripts are in the json file")		
+	else:
+		print("There are " + str(len(missing)) + " scripts that are missing from " +str(arg)+":")
+		print("\n".join(missing))
+		status = 1
+exit(status)
