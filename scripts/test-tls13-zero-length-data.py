5e74e2d3071a8809a85355e50d65f6d0aa666c6e
Nikos Mavrogiannopoulos <nmav@redhat.com>
2018-07-23 12:55:47 +0200

    test-tls13-finished/test-tls13-zero-length-data: allow new session ticket
    Previously the tests 'zero-length app data with padding during handshake'
    and 'zero-length app data during handshake' would fail if this ticket
    would be received.
    
    Signed-off-by: Nikos Mavrogiannopoulos <nmav@redhat.com>

--- scripts/test-tls13-zero-length-data.py
+++ scripts/test-tls13-zero-length-data.py
@@ -298,9 +298,16 @@ def main():
     node = node.add_child(ExpectCertificateVerify())
     node = node.add_child(ExpectFinished())
     node = node.add_child(ApplicationDataGenerator(bytearray(0)))
-    node = node.add_child(ExpectAlert(AlertLevel.fatal,
-                                      AlertDescription.unexpected_message))
-    node.add_child(ExpectClose())
+
+    # This message is optional and may show up 0 to many times
+    cycle = ExpectNewSessionTicket()
+    node = node.add_child(cycle)
+    node.add_child(cycle)
+
+    node.next_sibling = ExpectAlert(AlertLevel.fatal,
+                                    AlertDescription.unexpected_message)
+    node.next_sibling.add_child(ExpectClose())
+
     conversations["zero-length app data during handshake"] = conversation
 
     # Send zero-length application data while handshaking with padding
@@ -332,9 +339,16 @@ def main():
     node = node.add_child(SetPaddingCallback(
         SetPaddingCallback.fixed_length_cb(30)))
     node = node.add_child(ApplicationDataGenerator(bytearray(0)))
-    node = node.add_child(ExpectAlert(AlertLevel.fatal,
-                                      AlertDescription.unexpected_message))
-    node.add_child(ExpectClose())
+
+    # This message is optional and may show up 0 to many times
+    cycle = ExpectNewSessionTicket()
+    node = node.add_child(cycle)
+    node.add_child(cycle)
+
+    node.next_sibling = ExpectAlert(AlertLevel.fatal,
+                                    AlertDescription.unexpected_message)
+    node.next_sibling.add_child(ExpectClose())
+
     conversations["zero-length app data with padding during handshake"] =\
         conversation
 
