ac196b4a74eb487aeac44853f23ce4182d81be57
Hubert Kario <hkario@redhat.com>
2018-08-31 16:11:39 +0200

    method to test for uniquness of collected variables

--- tests/test_tlsfuzzer_helpers.py
+++ tests/test_tlsfuzzer_helpers.py
@@ -14,7 +14,8 @@ except ImportError:
 
 
 from tlsfuzzer.helpers import sig_algs_to_ids, key_share_gen, psk_ext_gen, \
-        flexible_getattr, psk_session_ext_gen, key_share_ext_gen
+        flexible_getattr, psk_session_ext_gen, key_share_ext_gen, \
+        uniquness_check
 from tlsfuzzer.runner import ConnectionState
 from tlslite.extensions import KeyShareEntry, PreSharedKeyExtension, \
         PskIdentity, ClientKeyShareExtension
@@ -193,3 +194,32 @@ class TestFlexibleGetattr(unittest.TestCase):
     def test_with_invalid_name(self):
         with self.assertRaises(AttributeError):
             flexible_getattr("seccc", GroupName)
+
+
+class TestUniqunessCheck(unittest.TestCase):
+    def test_with_empty(self):
+        self.assertEqual([], uniquness_check({}, 0))
+
+    def test_with_ints(self):
+        self.assertEqual([], uniquness_check({'ints': [1, 2, 3, 4]}, 4))
+
+    def test_with_duplicated_ints(self):
+        self.assertEqual(["Duplicated entries in 'ints'."],
+                         uniquness_check({'ints': [1, 2, 3, 1]}, 4))
+
+    def test_with_mismatched_count(self):
+        self.assertEqual(["Unexpected number of values in 'ints'. Expected: "
+                          "4, got: 3."],
+                         uniquness_check({'ints': [1, 2, 3]}, 4))
+
+    def test_with_bytearrays(self):
+        self.assertEqual(
+            [],
+            uniquness_check({'bytearrays':
+                             [bytearray(b'a'), bytearray(b'b')]}, 2))
+
+    def test_with_duplicated_bytearrays(self):
+        self.assertEqual(
+            ["Duplicated entries in 'bytearrays'."],
+            uniquness_check({'bytearrays':
+                             [bytearray(b'a'), bytearray(b'a')]}, 2))
