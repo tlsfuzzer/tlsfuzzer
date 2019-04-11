97116ae8057bcc938921384ef958cfd47f8a76a6
Hubert Kario <hkario@redhat.com>
2019-04-11 14:58:38 +0200

    TLSv1_2-rejected-without-TLSv1_2: modernise script, more sanity checks

--- scripts/test-TLSv1_2-rejected-without-TLSv1_2.py
+++ scripts/test-TLSv1_2-rejected-without-TLSv1_2.py
@@ -23,6 +23,9 @@ from tlslite.constants import CipherSuite, AlertLevel, AlertDescription, \
 from tlsfuzzer.utils.lists import natural_sort_keys
 
 
+version = 2
+
+
 def help_msg():
     print("Usage: <script-name> [-h hostname] [-p port] [[probe-name] ...]")
     print(" -h hostname    name of the host to run the test against")
@@ -33,7 +36,7 @@ def help_msg():
     print(" -e probe-name  exclude the probe from the list of the ones run")
     print("                may be specified multiple times")
     print(" -n num         only run `num` random tests instead of a full set")
-    print("                (excluding \"sanity\" tests)")
+    print("                (\"sanity\" tests are always executed)")
     print(" --help         this message")
 
 
@@ -76,6 +79,35 @@ def main():
                                                extensions={ExtensionType.
                                                    renegotiation_info:None}))
     node = node.add_child(ExpectServerHello(version=(3, 3),
+                                            extensions={ExtensionType.
+                                                     renegotiation_info:None}))
+    node = node.add_child(ExpectCertificate())
+    #node = node.add_child(ExpectServerKeyExchange())
+    node = node.add_child(ExpectServerHelloDone())
+    node = node.add_child(ClientKeyExchangeGenerator())
+    node = node.add_child(ChangeCipherSpecGenerator())
+    node = node.add_child(FinishedGenerator())
+    node = node.add_child(ExpectChangeCipherSpec())
+    node = node.add_child(ExpectFinished())
+    node = node.add_child(ApplicationDataGenerator(
+        bytearray(b"GET / HTTP/1.0\n\n")))
+    node = node.add_child(ExpectApplicationData())
+    node = node.add_child(AlertGenerator(AlertLevel.warning,
+                                         AlertDescription.close_notify))
+    node = node.add_child(ExpectAlert())
+    node.next_sibling = ExpectClose()
+
+    conversations["sanity"] = conversation
+
+    # sanity check - TLS 1.1 enabled
+    conversation = Connect(host, port)
+    node = conversation
+    ciphers = [CipherSuite.TLS_RSA_WITH_AES_128_CBC_SHA]
+    node = node.add_child(ClientHelloGenerator(ciphers,
+                                               version=(3, 2),
+                                               extensions={ExtensionType.
+                                                   renegotiation_info:None}))
+    node = node.add_child(ExpectServerHello(version=(3, 2),
                                             extensions={ExtensionType.
                                                      renegotiation_info:None}))
     node = node.add_child(ExpectCertificate())
@@ -144,7 +176,7 @@ def main():
         res = True
         try:
             runner.run()
-        except:
+        except Exception:
             print("Error while processing")
             print(traceback.format_exc())
             res = False
@@ -155,6 +187,10 @@ def main():
         else:
             bad += 1
             failed.append(c_name)
+
+    print("Script to verify that server does not negotiate TLS1.2 ciphers")
+    print("in TLS1.1 or earlier protocol")
+    print("version: {0}\n".format(version))
 
     print("Test end")
     print("successful: {0}".format(good))
