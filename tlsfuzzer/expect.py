c8ca5c87dcb2fbe11e1ba91448e3079dec812991
Hubert Kario <hkario@redhat.com>
2019-10-17 17:39:25 +0200

    reminder about CertificateRequest certificate_types field for expect

--- tlsfuzzer/expect.py
+++ tlsfuzzer/expect.py
@@ -1073,6 +1073,7 @@ class ExpectCertificateRequest(ExpectHandshake):
         super(ExpectCertificateRequest, self).__init__(ContentType.handshake,
                                                        msg_type)
         self.sig_algs = sig_algs
+        # TODO: ability to verify certificate_types field
 
     def process(self, state, msg):
         """
