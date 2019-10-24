51906607a06d11a90090e607efaec130a2316760
Hubert Kario <hkario@redhat.com>
2019-10-24 18:33:27 +0200

    ecdsa-sig-flexibility: make complete, enable
    ECDSA is now supported so the test case can be made complete
    (verify that the signatures made by other side are correct)
    also execute against tlslite-ng in regression tests
    
    modernise runner
    
    fixes #252
    fixes #259

--- scripts/test-ecdsa-sig-flexibility.py
+++ scripts/test-ecdsa-sig-flexibility.py
@@ -1,11 +1,10 @@
-# Author: Hubert Kario, (c) 2017
+# Author: Hubert Kario, (c) 2017,2019
 # Released under Gnu GPL v2.0, see LICENSE file for details
 
 from __future__ import print_function
 import traceback
 import sys
 import getopt
-import re
 from itertools import chain
 from random import sample
 
@@ -22,12 +21,11 @@ from tlslite.extensions import SignatureAlgorithmsExtension, \
         SupportedGroupsExtension, SignatureAlgorithmsCertExtension
 from tlslite.constants import CipherSuite, AlertLevel, AlertDescription, \
         HashAlgorithm, SignatureAlgorithm, ExtensionType, GroupName
-from tlsfuzzer.helpers import RSA_SIG_ALL
+from tlsfuzzer.helpers import SIG_ALL
+from tlsfuzzer.utils.lists import natural_sort_keys
 
 
-def natural_sort_keys(s, _nsre=re.compile('([0-9]+)')):
-    return [int(text) if text.isdigit() else text.lower()
-            for text in re.split(_nsre, s)]
+version = 2
 
 
 def help_msg():
@@ -39,16 +37,19 @@ def help_msg():
     print("                names and not all of them, e.g \"sanity\"")
     print(" -e probe-name  exclude the probe from the list of the ones run")
     print("                may be specified multiple times")
+    print(" -n num         only run `num` random tests instead of a full set")
+    print("                (\"sanity\" tests are always executed)")
     print(" --help         this message")
 
 
 def main():
     host = "localhost"
     port = 4433
+    num_limit = None
     run_exclude = set()
 
     argv = sys.argv[1:]
-    opts, args = getopt.getopt(argv, "h:p:e:", ["help"])
+    opts, args = getopt.getopt(argv, "h:p:e:n:", ["help"])
     for opt, arg in opts:
         if opt == '-h':
             host = arg
@@ -56,6 +57,8 @@ def main():
             port = int(arg)
         elif opt == '-e':
             run_exclude.add(arg)
+        elif opt == '-n':
+            num_limit = int(arg)
         elif opt == '--help':
             help_msg()
             sys.exit(0)
@@ -73,13 +76,14 @@ def main():
     node = conversation
     ext = {}
     sigalgs = [(HashAlgorithm.sha1, SignatureAlgorithm.ecdsa),
+               (HashAlgorithm.sha224, SignatureAlgorithm.ecdsa),
                (HashAlgorithm.sha256, SignatureAlgorithm.ecdsa),
                (HashAlgorithm.sha384, SignatureAlgorithm.ecdsa),
                (HashAlgorithm.sha512, SignatureAlgorithm.ecdsa)]
     ext[ExtensionType.signature_algorithms] = \
             SignatureAlgorithmsExtension().create(sigalgs)
     ext[ExtensionType.signature_algorithms_cert] = \
-            SignatureAlgorithmsCertExtension().create(RSA_SIG_ALL)
+            SignatureAlgorithmsCertExtension().create(SIG_ALL)
     curves = [GroupName.secp256r1,
               GroupName.secp384r1,
               GroupName.secp521r1]
@@ -90,22 +94,21 @@ def main():
     node = node.add_child(ClientHelloGenerator(ciphers, version=(3, 3),
                                                extensions=ext))
     node = node.add_child(ExpectServerHello())
-    # ECDSA is not supported by tlslite-ng v0.7.0a3
-    #node = node.add_child(ExpectCertificate())
-    #node = node.add_child(ExpectServerKeyExchange())
-    #node = node.add_child(ExpectServerHelloDone())
-    #node = node.add_child(ClientKeyExchangeGenerator())
-    #node = node.add_child(ChangeCipherSpecGenerator())
-    #node = node.add_child(FinishedGenerator())
-    #node = node.add_child(ExpectChangeCipherSpec())
-    #node = node.add_child(ExpectFinished())
-    #node = node.add_child(ApplicationDataGenerator(
-    #    bytearray(b"GET / HTTP/1.0\n\n")))
-    #node = node.add_child(ExpectApplicationData())
-    #node = node.add_child(AlertGenerator(AlertLevel.warning,
-    #                                     AlertDescription.close_notify))
-    #node = node.add_child(ExpectAlert())
-    #node.next_sibling = ExpectClose()
+    node = node.add_child(ExpectCertificate())
+    node = node.add_child(ExpectServerKeyExchange())
+    node = node.add_child(ExpectServerHelloDone())
+    node = node.add_child(ClientKeyExchangeGenerator())
+    node = node.add_child(ChangeCipherSpecGenerator())
+    node = node.add_child(FinishedGenerator())
+    node = node.add_child(ExpectChangeCipherSpec())
+    node = node.add_child(ExpectFinished())
+    node = node.add_child(ApplicationDataGenerator(
+        bytearray(b"GET / HTTP/1.0\n\n")))
+    node = node.add_child(ExpectApplicationData())
+    node = node.add_child(AlertGenerator(AlertLevel.warning,
+                                         AlertDescription.close_notify))
+    node = node.add_child(ExpectAlert())
+    node.next_sibling = ExpectClose()
     conversations["sanity"] = conversation
 
     # all algorithms, but in order from strongest to weakest
@@ -115,11 +118,12 @@ def main():
     sigalgs = [(HashAlgorithm.sha512, SignatureAlgorithm.ecdsa),
                (HashAlgorithm.sha384, SignatureAlgorithm.ecdsa),
                (HashAlgorithm.sha256, SignatureAlgorithm.ecdsa),
+               (HashAlgorithm.sha224, SignatureAlgorithm.ecdsa),
                (HashAlgorithm.sha1, SignatureAlgorithm.ecdsa)]
     ext[ExtensionType.signature_algorithms] = \
             SignatureAlgorithmsExtension().create(sigalgs)
     ext[ExtensionType.signature_algorithms_cert] = \
-            SignatureAlgorithmsCertExtension().create(RSA_SIG_ALL)
+            SignatureAlgorithmsCertExtension().create(SIG_ALL)
     curves = [GroupName.secp256r1,
               GroupName.secp384r1,
               GroupName.secp521r1]
@@ -130,26 +134,25 @@ def main():
     node = node.add_child(ClientHelloGenerator(ciphers, version=(3, 3),
                                                extensions=ext))
     node = node.add_child(ExpectServerHello())
-    # ECDSA is not supported by tlslite-ng v0.7.0a3
-    #node = node.add_child(ExpectCertificate())
-    #node = node.add_child(ExpectServerKeyExchange())
-    #node = node.add_child(ExpectServerHelloDone())
-    #node = node.add_child(ClientKeyExchangeGenerator())
-    #node = node.add_child(ChangeCipherSpecGenerator())
-    #node = node.add_child(FinishedGenerator())
-    #node = node.add_child(ExpectChangeCipherSpec())
-    #node = node.add_child(ExpectFinished())
-    #node = node.add_child(ApplicationDataGenerator(
-    #    bytearray(b"GET / HTTP/1.0\n\n")))
-    #node = node.add_child(ExpectApplicationData())
-    #node = node.add_child(AlertGenerator(AlertLevel.warning,
-    #                                     AlertDescription.close_notify))
-    #node = node.add_child(ExpectAlert())
-    #node.next_sibling = ExpectClose()
+    node = node.add_child(ExpectCertificate())
+    node = node.add_child(ExpectServerKeyExchange())
+    node = node.add_child(ExpectServerHelloDone())
+    node = node.add_child(ClientKeyExchangeGenerator())
+    node = node.add_child(ChangeCipherSpecGenerator())
+    node = node.add_child(FinishedGenerator())
+    node = node.add_child(ExpectChangeCipherSpec())
+    node = node.add_child(ExpectFinished())
+    node = node.add_child(ApplicationDataGenerator(
+        bytearray(b"GET / HTTP/1.0\n\n")))
+    node = node.add_child(ExpectApplicationData())
+    node = node.add_child(AlertGenerator(AlertLevel.warning,
+                                         AlertDescription.close_notify))
+    node = node.add_child(ExpectAlert())
+    node.next_sibling = ExpectClose()
     conversations["sanity - strongest to weakest"] = conversation
 
     # test all hashes one by one
-    for h_alg in ["sha1", "sha256", "sha384", "sha512"]:
+    for h_alg in ["sha1", "sha224", "sha256", "sha384", "sha512"]:
         conversation = Connect(host, port)
         node = conversation
         ext = {}
@@ -157,7 +160,7 @@ def main():
         ext[ExtensionType.signature_algorithms] = \
                 SignatureAlgorithmsExtension().create(sigalgs)
         ext[ExtensionType.signature_algorithms_cert] = \
-                SignatureAlgorithmsCertExtension().create(RSA_SIG_ALL)
+                SignatureAlgorithmsCertExtension().create(SIG_ALL)
         curves = [GroupName.secp256r1,
                   GroupName.secp384r1,
                   GroupName.secp521r1]
@@ -168,35 +171,36 @@ def main():
         node = node.add_child(ClientHelloGenerator(ciphers, version=(3, 3),
                                                    extensions=ext))
         node = node.add_child(ExpectServerHello())
-        # ECDSA is not supported by tlslite-ng v0.7.0a3
-        #node = node.add_child(ExpectCertificate())
-        #node = node.add_child(ExpectServerKeyExchange())
-        #node = node.add_child(ExpectServerHelloDone())
-        #node = node.add_child(ClientKeyExchangeGenerator())
-        #node = node.add_child(ChangeCipherSpecGenerator())
-        #node = node.add_child(FinishedGenerator())
-        #node = node.add_child(ExpectChangeCipherSpec())
-        #node = node.add_child(ExpectFinished())
-        #node = node.add_child(ApplicationDataGenerator(
-        #    bytearray(b"GET / HTTP/1.0\n\n")))
-        #node = node.add_child(ExpectApplicationData())
-        #node = node.add_child(AlertGenerator(AlertLevel.warning,
-        #                                     AlertDescription.close_notify))
-        #node = node.add_child(ExpectAlert())
-        #node.next_sibling = ExpectClose()
+        node = node.add_child(ExpectCertificate())
+        node = node.add_child(ExpectServerKeyExchange())
+        node = node.add_child(ExpectServerHelloDone())
+        node = node.add_child(ClientKeyExchangeGenerator())
+        node = node.add_child(ChangeCipherSpecGenerator())
+        node = node.add_child(FinishedGenerator())
+        node = node.add_child(ExpectChangeCipherSpec())
+        node = node.add_child(ExpectFinished())
+        node = node.add_child(ApplicationDataGenerator(
+            bytearray(b"GET / HTTP/1.0\n\n")))
+        node = node.add_child(ExpectApplicationData())
+        node = node.add_child(AlertGenerator(AlertLevel.warning,
+                                             AlertDescription.close_notify))
+        node = node.add_child(ExpectAlert())
+        node.next_sibling = ExpectClose()
         conversations["connect with {0}+ecdsa only".format(h_alg)] = conversation
 
     # run the conversation
     good = 0
     bad = 0
     failed = []
+    if not num_limit:
+        num_limit = len(conversations)
 
     # make sure that sanity test is run first and last
     # to verify that server was running and kept running throughout
     sanity_tests = [('sanity', conversations['sanity'])]
     regular_tests = [(k, v) for k, v in conversations.items() if k != 'sanity']
-    shuffled_tests = sample(regular_tests, len(regular_tests))
-    ordered_tests = chain(sanity_tests, shuffled_tests, sanity_tests)
+    sampled_tests = sample(regular_tests, min(num_limit, len(regular_tests)))
+    ordered_tests = chain(sanity_tests, sampled_tests, sanity_tests)
 
     for c_name, c_test in ordered_tests:
         if run_only and c_name not in run_only or c_name in run_exclude:
@@ -208,7 +212,7 @@ def main():
         res = True
         try:
             runner.run()
-        except:
+        except Exception:
             print("Error while processing")
             print(traceback.format_exc())
             res = False
@@ -222,6 +226,7 @@ def main():
 
     print("Test if the server doesn't have the TLSv1.3 limitation on ECDSA")
     print("signatures also in TLSv1.2 mode.\n")
+    print("version: {0}\n".format(version))
 
     print("Test end")
     print("successful: {0}".format(good))
