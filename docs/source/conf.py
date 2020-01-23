bcd7101eb37e9236479c78b5ae8184d2eaadde7a
Hubert Kario <hkario@redhat.com>
2020-01-23 20:36:38 +0100

    link to tlslite-ng documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -183,4 +183,5 @@ epub_exclude_files = ['search.html']
 # -- Options for intersphinx extension ---------------------------------------
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {'https://docs.python.org/': None,
+                       'https://tlslite-ng.readthedocs.io/en/latest/': None}
