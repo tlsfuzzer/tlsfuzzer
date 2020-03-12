8e489b822989dadbc1bc0a4f5b7551354ad124b2
KrenzelokFrantisek <krenzelok.frantisek@gmail.com>
2020-03-12 14:41:42 +0100

    Modified the way sample tests are chosen When run only and -n specified -> run -n test from run_only list...

--- scripts/test-dhe-rsa-key-exchange.py
+++ scripts/test-dhe-rsa-key-exchange.py
@@ -105,8 +105,14 @@ def main():
     # make sure that sanity test is run first and last
     # to verify that server was running and kept running throughout
     sanity_tests = [('sanity', conversations['sanity'])]
-    regular_tests = [(k, v) for k, v in conversations.items() if k != 'sanity']
-    sampled_tests = sample(regular_tests, min(num_limit, len(regular_tests)))
+    if run_only:
+        if num_limit > len(run_only):
+            num_limit = len(run_only)
+        run_only_tests = [(k, v) for k, v in conversations.items() if (k in run_only)]
+        sampled_tests = sample(run_only_tests, min(num_limit, len(run_only)))
+    else:
+        regular_tests = [(k, v) for k, v in conversations.items() if (k != 'sanity') and (k not in run_exclude)]
+        sampled_tests = sample(regular_tests, min(num_limit, len(regular_tests)))
     ordered_tests = chain(sanity_tests, sampled_tests, sanity_tests)
 
     for c_name, c_test in ordered_tests:
