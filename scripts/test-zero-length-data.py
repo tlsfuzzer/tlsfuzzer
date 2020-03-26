e4bb3fc976abc766a658cf5845fe4a2aa9af3f4e
KrenzelokFrantisek <krenzelok.frantisek@gmail.com>
2020-03-26 16:39:30 +0100

    Implementing `-x` to all test Adding help messages, `-x`, `-X`, sometimes `-e` options and editing test transcript message

--- scripts/test-zero-length-data.py
+++ scripts/test-zero-length-data.py
@@ -6,6 +6,7 @@ from __future__ import print_function
 import traceback
 from random import sample
 import sys
+import re
 import getopt
 
 from tlsfuzzer.runner import Runner
@@ -18,13 +19,21 @@ from tlsfuzzer.expect import ExpectServerHello, ExpectCertificate, \
         ExpectAlert, ExpectClose, ExpectApplicationData
 
 from tlslite.constants import CipherSuite, AlertLevel, AlertDescription
-
+from tlsfuzzer.utils.lists import natural_sort_keys
 
 def help_msg():
     print("Usage: <script-name> [-h hostname] [-p port] [[probe-name] ...]")
     print(" -h hostname    name of the host to run the test against")
     print("                localhost by default")
     print(" -p port        port number to use for connection, 4433 by default")
+    print(" -e probe-name  exclude the probe from the list of the ones run")
+    print("                may be specified multiple times")
+    print(" -x probe-name  expect the probe to fail. When such probe passes despite being marked like this")
+    print("                it will be reported in the test summary and the whole script will fail.")
+    print("                May be specified multiple times.")
+    print(" -X message     expect the `message` substring in exception raised during")
+    print("                execution of preceding expected failure probe")
+    print("                usage: [-x probe-name] [-X exception], order is compulsory!")
     print(" --help         this message")
 
 
@@ -33,9 +42,12 @@ def main():
     conversations = {}
     host = "localhost"
     port = 4433
+    run_exclude = set()
+    expected_failures = {}
+    last_exp_tmp = None
 
     argv = sys.argv[1:]
-    opts, argv = getopt.getopt(argv, "h:p:", ["help"])
+    opts, argv = getopt.getopt(argv, "h:p:e:x:X:", ["help"])
     for opt, arg in opts:
         if opt == '-h':
             host = arg
@@ -44,6 +56,15 @@ def main():
         elif opt == '--help':
             help_msg()
             sys.exit(0)
+        elif opt == '-e':
+            run_exclude.add(arg)
+        elif opt == '-x':
+            expected_failures[arg] = None
+            last_exp_tmp = str(arg)
+        elif opt == '-X':
+            if not last_exp_tmp:
+                raise ValueError("-x has to be specified before -X")
+            expected_failures[last_exp_tmp] = str(arg)
         else:
             raise ValueError("Unknown option: {0}".format(opt))
     if argv:
@@ -80,35 +101,71 @@ def main():
     # run the conversation
     good = 0
     bad = 0
+    xfail = 0
+    xpass = 0
+    failed = []
+    xpassed = []
+    sampled_tests = sample(list(conversations.items()), len(conversations))
 
-    shuffled_tests = sample(list(conversations.items()), len(conversations))
+    for c_name, conversation in sampled_tests:
+        if c_name in run_exclude:
+            continue
 
-    for conversation_name, conversation in shuffled_tests:
-        print("{0} ...".format(conversation_name))
+        print("{0} ...".format(c_name))
 
         runner = Runner(conversation)
 
         res = True
+        exception = None
         #because we don't want to abort the testing and we are reporting
         #the errors to the user, using a bare except is OK
         #pylint: disable=bare-except
         try:
             runner.run()
-        except:
+        except Exception as exp:
+            exception = exp
             print("Error while processing")
             print(traceback.format_exc())
             res = False
         #pylint: enable=bare-except
 
-        if res:
-            good+=1
-            print("OK")
+        if c_name in expected_failures:
+            if res:
+                xpass += 1
+                xpassed.append(c_name)
+                print("XPASS: expected failure but test passed\n")
+            else:
+                if expected_failures[c_name] is not None and  \
+                    expected_failures[c_name] not in str(exception):
+                        bad += 1
+                        failed.append(c_name)
+                        print("Expected error message: {0}\n"
+                            .format(expected_failures[c_name]))
+                else:
+                    xfail += 1
+                    print("OK-expected failure\n")
         else:
-            bad+=1
-
+            if res:
+                good+=1
+                print("OK")
+            else:
+                bad+=1
+    
     print("Test end")
-    print("successful: {0}".format(good))
-    print("failed: {0}".format(bad))
+    print(20 * '=')
+    print("TOTAL: {0}".format(len(sampled_tests)))
+    print("SKIP: {0}".format(len(run_exclude.intersection(conversations.keys()))))
+    print("PASS: {0}".format(good))
+    print("XFAIL: {0}".format(xfail))
+    print("FAIL: {0}".format(bad))
+    print("XPASS: {0}".format(xpass))
+    print(20 * '=')
+    sort = sorted(xpassed ,key=natural_sort_keys)
+    if len(sort):
+        print("XPASSED:\n\t{0}".format('\n\t'.join(repr(i) for i in sort)))
+    sort = sorted(failed, key=natural_sort_keys)
+    if len(sort):
+        print("FAILED:\n\t{0}".format('\n\t'.join(repr(i) for i in sort)))
 
     if bad > 0:
         sys.exit(1)
