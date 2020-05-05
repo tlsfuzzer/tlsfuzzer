d89ecf3f7721de29e87659337d909bff496f548f
Jan Koscielniak <jkosci@gmail.com>
2020-05-05 16:58:16 +0200

    Add documentation, make changes according to code review
    Changelog:
     - rewrote Log and other parts to allow for random order of tests
     - removed unnecessary wait and cmdline arguments for tcpdump
     - moved the example directly onto test-bleichenbacher-workaround
     - rewrote analysis to use getopt to preserve compatibility
     - changed next method in Analysis to be compatible with python 2

--- tlsfuzzer/utils/log.py
+++ tlsfuzzer/utils/log.py
@@ -3,11 +3,12 @@
 
 """Utilities for logging the conversations for later timing analysis."""
 
-from itertools import cycle
+import json
+from random import shuffle
 
 
 class Log:
-    """Class that provides tools to interact with log files for later timing analysis."""
+    """Tools to interact with log files for later timing analysis."""
 
     def __init__(self, filename):
         """
@@ -16,31 +17,53 @@ class Log:
         :param str filename: log filename
         """
         self.filename = filename
-        self.log = []
+        self.log = {
+            "classes": [],
+            "runs": []
+        }
 
-    def add_class_name(self, class_name):
+    def set_classes_list(self, class_list):
         """
-        Inserts class name into internal list.
-        :param str class_name: Class name to identify connection with
+        Inserts list of classes name into internal list.
+        :param list class_list: List of test classes to be used to identify a
+            connection later during the analysis
         """
-        self.log.append(class_name)
+        self.log["classes"] = class_list
+
+    def add_run(self, run):
+        """
+        Appends a new run order
+        :param list run: List of (shuffled) indexes to the "classes" list
+        """
+        self.log["runs"].append(run)
+
+    def shuffle_new_run(self):
+        """
+        Generates a new test order as an index to the "classes" list
+        """
+        original_order = list(range(0, len(self.log["classes"])))
+        shuffle(original_order)
+        self.add_run(original_order)
 
     def write_log(self):
         """Write collected log information into a file."""
         with open(self.filename, 'w') as logfile:
-            for line in self.log:
-                logfile.write(line)
-                logfile.write('\n')
+            json.dump(self.log, logfile)
 
     def read_log(self):
         """Read classes from log file into internal list."""
-        log = []
         with open(self.filename, 'r') as logfile:
-            for line in logfile:
-                log.append(line.rstrip("\n"))
-        self.log = log
+            self.log = json.load(logfile)
+
+    def get_classes(self):
+        """
+        Get class names from current log
+        :return: list  Class names list
+        """
+        return self.log["classes"]
 
     def iterate_log(self):
-        """Provide a generator to iterate over classes infinitely."""
-        for item in cycle(self.log):
-            yield item
+        """Provide a generator to iterate over runs."""
+        for run in self.log["runs"]:
+            for index in run:
+                yield index
