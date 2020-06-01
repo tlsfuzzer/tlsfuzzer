7fa194312fda65fa46858abf244fc9c5f48158d9
Jan Koscielniak <jkosci@gmail.com>
2020-06-01 23:35:06 +0200

    Restrict to a single ciphersuite, separate into classes by time, remove correct padding

--- scripts/test-lucky13.py
+++ scripts/test-lucky13.py
@@ -1,4 +1,4 @@
-# Author: Hubert Kario, (c) 2015-2018
+# Author: Jan Koscielniak, (c) 2020
 # Released under Gnu GPL v2.0, see LICENSE file for details
 
 """Tests for Lucky 13 vulnerabilities and related conditions"""
@@ -46,6 +46,10 @@ def help_msg():
     print(" -X message     expect the `message` substring in exception raised during")
     print("                execution of preceding expected failure probe")
     print("                usage: [-x probe-name] [-X exception], order is compulsory!")
+    print(" -C cipher      specify cipher for connection. Use integer value")
+    print("                or IETF name. Integer must be prefixed with '0x'")
+    print("                if it is hexadecimal. By default uses")
+    print("                TLS_RSA_WITH_AES_128_CBC_SHA ciphersuite.")
     print(" -n num         only run `num` random tests instead of a full set")
     print("                (\"sanity\" tests are always executed)")
     print(" -i interface   Allows recording timing information on")
@@ -55,6 +59,7 @@ def help_msg():
     print(" --repeat rep   How many timing samples should be gathered for each test")
     print("                100 by default")
     print(" --help         this message")
+
 
 def main():
     host = "localhost"
@@ -67,9 +72,10 @@ def main():
     outdir = "/tmp"
     repetitions = 100
     interface = None
+    cipher = CipherSuite.TLS_RSA_WITH_AES_128_CBC_SHA
 
     argv = sys.argv[1:]
-    opts, args = getopt.getopt(argv, "h:p:e:x:X:n:l:o:i:", ["help", "repeat="])
+    opts, args = getopt.getopt(argv, "h:p:e:x:X:n:l:o:i:C:", ["help", "repeat="])
     for opt, arg in opts:
         if opt == '-h':
             host = arg
@@ -84,6 +90,14 @@ def main():
             if not last_exp_tmp:
                 raise ValueError("-x has to be specified before -X")
             expected_failures[last_exp_tmp] = str(arg)
+        elif opt == '-C':
+            if arg[:2] == '0x':
+                cipher = int(arg, 16)
+            else:
+                try:
+                    cipher = getattr(CipherSuite, arg)
+                except AttributeError:
+                    cipher = int(arg)
         elif opt == '-n':
             num_limit = int(arg)
         elif opt == '-l':
@@ -113,32 +127,78 @@ def main():
         "sha384": 48,
     }
 
-    block_sizes = {
-        "3des": 8,
-        "aes128": 16,
-        "aes256": 16,
+    if CipherSuite.canonicalMacName(cipher) not in tag_sizes:
+        print("Unsupported MAC, exiting")
+        exit(1)
+
+    classes = {
+        "wrong padding and mac": {},
+        "mac out of bounds": {}
     }
-    ciphersuites = chain(CipherSuite.tripleDESSuites, CipherSuite.aes128Suites, CipherSuite.aes256Suites)
-    for cipher in ciphersuites:
-        if CipherSuite.canonicalMacName(cipher) not in tag_sizes:
-            continue
+    dhe = cipher in CipherSuite.ecdhAllSuites
+    if dhe:
+        ext = {}
+        groups = [GroupName.secp256r1,
+                  GroupName.ffdhe2048]
+        ext[ExtensionType.supported_groups] = SupportedGroupsExtension() \
+            .create(groups)
+        ext[ExtensionType.signature_algorithms] = \
+            SignatureAlgorithmsExtension().create(SIG_ALL)
+        ext[ExtensionType.signature_algorithms_cert] = \
+            SignatureAlgorithmsCertExtension().create(SIG_ALL)
+    else:
+        ext = None
 
-        conversations = {}
-        dhe = cipher in CipherSuite.ecdhAllSuites
-        if dhe:
-            ext = {}
-            groups = [GroupName.secp256r1,
-                      GroupName.ffdhe2048]
-            ext[ExtensionType.supported_groups] = SupportedGroupsExtension() \
-                .create(groups)
-            ext[ExtensionType.signature_algorithms] = \
-                SignatureAlgorithmsExtension().create(SIG_ALL)
-            ext[ExtensionType.signature_algorithms_cert] = \
-                SignatureAlgorithmsCertExtension().create(SIG_ALL)
-        else:
-            ext = None
+    # first run sanity test and verify that server supports this ciphersuite
+    conversation = Connect(host, port)
+    node = conversation
+    ciphers = [cipher]
+    node = node.add_child(ClientHelloGenerator(ciphers, extensions=ext))
+    node = node.add_child(ExpectServerHello())
+    node = node.add_child(ExpectCertificate())
+    if dhe:
+        node = node.add_child(ExpectServerKeyExchange())
+    node = node.add_child(ExpectServerHelloDone())
+    node = node.add_child(ClientKeyExchangeGenerator())
+    node = node.add_child(ChangeCipherSpecGenerator())
+    node = node.add_child(FinishedGenerator())
+    node = node.add_child(ExpectChangeCipherSpec())
+    node = node.add_child(ExpectFinished())
+    node = node.add_child(ApplicationDataGenerator(b"GET / HTTP/1.0\r\n\r\n"))
+    node = node.add_child(ExpectApplicationData())
+    node = node.add_child(AlertGenerator(AlertLevel.warning,
+                                         AlertDescription.close_notify))
+    node = node.add_child(ExpectAlert(AlertLevel.warning,
+                                      AlertDescription.close_notify))
+    node.next_sibling = ExpectClose()
+    node = node.add_child(ExpectClose())
+    for class_name in classes:
+        classes[class_name]["sanity"] = conversation
+    runner = Runner(conversation)
+    try:
+        runner.run()
+    except Exception as exp:
+        # no exception means the server rejected the ciphersuite, skip to next ciphersuite
+        print("Skipping {0} because server does not support it. ".format(CipherSuite.ietfNames[cipher]))
+        print(20 * '=')
+        exit(0)
 
-        # first run sanity test and verify that server supports this ciphersuite
+    # assume block length of 16 if not 3des
+    block_len = 8 if CipherSuite.canonicalCipherName(cipher) == "3des" else 16
+    mac_len = tag_sizes[CipherSuite.canonicalMacName(cipher)]
+    invert_mac = {}
+    for index in range(0, mac_len):
+        invert_mac[index] = 0xff
+
+    # iterate over: incorrect padding
+    for pad_len in range(1, 256):
+
+        # ciphertext 1 has 512 bytes, calculate payload size
+        payload_len = 512 - mac_len - pad_len - block_len
+
+        sub = 0
+        if pad_len == 1:
+            sub = 0xff
         conversation = Connect(host, port)
         node = conversation
         ciphers = [cipher]
@@ -153,26 +213,16 @@ def main():
         node = node.add_child(FinishedGenerator())
         node = node.add_child(ExpectChangeCipherSpec())
         node = node.add_child(ExpectFinished())
-        node = node.add_child(ApplicationDataGenerator(b"GET / HTTP/1.0\r\n\r\n"))
-        node = node.add_child(ExpectApplicationData())
-        node = node.add_child(AlertGenerator(AlertLevel.warning,
-                                             AlertDescription.close_notify))
-        node = node.add_child(ExpectAlert(AlertLevel.warning,
-                                          AlertDescription.close_notify))
-        # sending of the close_notify reply to a close_notify is very unrealiable
-        # ignore it not being sent
-        node.next_sibling = ExpectClose()
-        conversations["sanity"] = conversation
-        runner = Runner(conversation)
-        try:
-            runner.run()
-        except Exception as exp:
-            # no exception means the server rejected the ciphersuite, skip to next ciphersuite
-            print("Skipping {0} because server does not support it. ".format(CipherSuite.ietfNames[cipher]))
-            print(20 * '=')
-            continue
+        node = node.add_child(
+            fuzz_padding(ApplicationDataGenerator(bytearray(payload_len)), substitutions={0: sub},
+                         min_length=pad_len))
+        node = node.add_child(ExpectAlert(AlertLevel.fatal,
+                                          AlertDescription.bad_record_mac))
+        node = node.add_child(ExpectClose())
+        classes["wrong padding and mac"]["incorrect padding of length {0}".format(pad_len)] = conversation
 
-        # correct padding for comparison
+        # ciphertext 2 has 128 bytes and broken padding to make server check mac "before" the plaintext
+        padding = 128 - 1 - block_len - mac_len
         conversation = Connect(host, port)
         node = conversation
         ciphers = [cipher]
@@ -187,98 +237,43 @@ def main():
         node = node.add_child(FinishedGenerator())
         node = node.add_child(ExpectChangeCipherSpec())
         node = node.add_child(ExpectFinished())
-        node = node.add_child(ApplicationDataGenerator(b"GET / HTTP/1.0\r\n\r\n"))
-        node = node.add_child(ExpectApplicationData())
-        node = node.add_child(AlertGenerator(AlertLevel.warning,
-                                             AlertDescription.close_notify))
-        node = node.add_child(ExpectAlert(AlertLevel.warning,
-                                          AlertDescription.close_notify))
-        # sending of the close_notify reply to a close_notify is very unrealiable
-        # ignore it not being sent
-        node.next_sibling = ExpectClose()
-        conversations["correct padding"] = conversation
+        node = node.add_child(
+            fuzz_padding(ApplicationDataGenerator(bytearray(1)), substitutions={-1: pad_len, 0: 0},
+                         min_length=padding))
+        node = node.add_child(ExpectAlert(AlertLevel.fatal,
+                                          AlertDescription.bad_record_mac))
+        node = node.add_child(ExpectClose())
+        classes["mac out of bounds"]["padding length byte={0}".format(pad_len)] = conversation
 
-        mac_len = tag_sizes[CipherSuite.canonicalMacName(cipher)]
-        block_len = block_sizes[CipherSuite.canonicalCipherName(cipher)]
-        invert_mac = {}
-        for index in range(0, mac_len):
-            invert_mac[index] = 0xff
+    # iterate over payload length to shift incorrect mac around
+    min_payload_len = 512 - mac_len - 255 - block_len
+    for payload_len in range(min_payload_len, 512 - mac_len - block_len):
+        # wrong mac at different position with correct padding
+        padding = 512 - mac_len - block_len - payload_len
+        conversation = Connect(host, port)
+        node = conversation
+        ciphers = [cipher]
+        node = node.add_child(ClientHelloGenerator(ciphers, extensions=ext))
+        node = node.add_child(ExpectServerHello())
+        node = node.add_child(ExpectCertificate())
+        if dhe:
+            node = node.add_child(ExpectServerKeyExchange())
+        node = node.add_child(ExpectServerHelloDone())
+        node = node.add_child(ClientKeyExchangeGenerator())
+        node = node.add_child(ChangeCipherSpecGenerator())
+        node = node.add_child(FinishedGenerator())
+        node = node.add_child(ExpectChangeCipherSpec())
+        node = node.add_child(ExpectFinished())
+        node = node.add_child(
+            fuzz_padding(fuzz_mac(ApplicationDataGenerator(bytearray(payload_len)), xors=invert_mac),
+                         min_length=padding))
+        node = node.add_child(ExpectAlert(AlertLevel.fatal,
+                                          AlertDescription.bad_record_mac))
+        node = node.add_child(ExpectClose())
+        classes["wrong padding and mac"][
+            "incorrect mac at pos {0}, padding of length {1}".format((payload_len - 1), padding)] = conversation
 
-        # iterate over: incorrect padding, correct padding and wrong mac
-        for pad_len in range(1, 256):
-
-            # ciphertext 1 has 512 bytes, calculate payload size
-            payload_len = 512 - mac_len - pad_len - block_len
-
-            # skip padding of length 1 because that would be correct padding
-            if pad_len != 1:
-                conversation = Connect(host, port)
-                node = conversation
-                ciphers = [cipher]
-                node = node.add_child(ClientHelloGenerator(ciphers, extensions=ext))
-                node = node.add_child(ExpectServerHello())
-                node = node.add_child(ExpectCertificate())
-                if dhe:
-                    node = node.add_child(ExpectServerKeyExchange())
-                node = node.add_child(ExpectServerHelloDone())
-                node = node.add_child(ClientKeyExchangeGenerator())
-                node = node.add_child(ChangeCipherSpecGenerator())
-                node = node.add_child(FinishedGenerator())
-                node = node.add_child(ExpectChangeCipherSpec())
-                node = node.add_child(ExpectFinished())
-                node = node.add_child(
-                    fuzz_padding(ApplicationDataGenerator(bytearray(payload_len)), substitutions={0: 0},
-                                 min_length=pad_len))
-                node = node.add_child(ExpectAlert(AlertLevel.fatal,
-                                                  AlertDescription.bad_record_mac))
-                node.next_sibling = ExpectClose()
-                conversations["incorrect padding of length {0}".format(pad_len)] = conversation
-
-            conversation = Connect(host, port)
-            node = conversation
-            ciphers = [cipher]
-            node = node.add_child(ClientHelloGenerator(ciphers, extensions=ext))
-            node = node.add_child(ExpectServerHello())
-            node = node.add_child(ExpectCertificate())
-            if dhe:
-                node = node.add_child(ExpectServerKeyExchange())
-            node = node.add_child(ExpectServerHelloDone())
-            node = node.add_child(ClientKeyExchangeGenerator())
-            node = node.add_child(ChangeCipherSpecGenerator())
-            node = node.add_child(FinishedGenerator())
-            node = node.add_child(ExpectChangeCipherSpec())
-            node = node.add_child(ExpectFinished())
-            node = node.add_child(
-                fuzz_padding(fuzz_mac(ApplicationDataGenerator(bytearray(payload_len)), xors=invert_mac),
-                             min_length=pad_len))
-            node = node.add_child(ExpectAlert(AlertLevel.fatal,
-                                              AlertDescription.bad_record_mac))
-            node.next_sibling = ExpectClose()
-            conversations["incorrect mac, padding of length {0}".format(pad_len)] = conversation
-
-            padding = 128 - 1 - block_len - mac_len
-            # ciphertext 2 has 128 bytes and broken padding to make server check mac "before" the plaintext
-            conversation = Connect(host, port)
-            node = conversation
-            ciphers = [cipher]
-            node = node.add_child(ClientHelloGenerator(ciphers, extensions=ext))
-            node = node.add_child(ExpectServerHello())
-            node = node.add_child(ExpectCertificate())
-            if dhe:
-                node = node.add_child(ExpectServerKeyExchange())
-            node = node.add_child(ExpectServerHelloDone())
-            node = node.add_child(ClientKeyExchangeGenerator())
-            node = node.add_child(ChangeCipherSpecGenerator())
-            node = node.add_child(FinishedGenerator())
-            node = node.add_child(ExpectChangeCipherSpec())
-            node = node.add_child(ExpectFinished())
-            node = node.add_child(
-                fuzz_padding(ApplicationDataGenerator(bytearray(1)), substitutions={-1: pad_len, 0: 0},
-                             min_length=padding))
-            node = node.add_child(ExpectAlert(AlertLevel.fatal,
-                                              AlertDescription.bad_record_mac))
-            node.next_sibling = ExpectClose()
-            conversations["mac out of bounds, padding byte={0}".format(pad_len)] = conversation
+    for class_name, conversations in classes.items():
 
         # run the conversation
         good = 0
@@ -339,7 +334,7 @@ def main():
                     bad += 1
                     failed.append(c_name)
 
-        print("Lucky 13 distinguishing attack check for {0}".format(CipherSuite.ietfNames[cipher]))
+        print("Lucky 13 attack check for {0} {1}".format(class_name, CipherSuite.ietfNames[cipher]))
 
         print("Test end")
         print(20 * '=')
@@ -362,7 +357,9 @@ def main():
         elif timing:
             # if regular tests passed, run timing collection and analysis
             if TimingRunner.check_tcpdump():
-                timing_runner = TimingRunner("{0}_{1}".format(sys.argv[0], CipherSuite.ietfNames[cipher]),
+                timing_runner = TimingRunner("{0}_{1}_{2}".format(sys.argv[0],
+                                                                  class_name,
+                                                                  CipherSuite.ietfNames[cipher]),
                                              sampled_tests,
                                              outdir,
                                              host,
