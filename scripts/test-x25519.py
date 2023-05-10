3d9724b9616a800fbdfed0b6c87cb2098909c3fc
Noam Rathaus <noamr@ssd-disclosure.com>
2023-05-10 10:11:54 +0300

    bad indentation

--- scripts/test-x25519.py
+++ scripts/test-x25519.py
@@ -1055,12 +1055,11 @@ def main():
                 xpassed.append(c_name)
                 print("XPASS-expected failure but test passed\n")
             else:
-                if expected_failures[c_name] is not None and  \
-                    expected_failures[c_name] not in str(exception):
-                        bad += 1
-                        failed.append(c_name)
-                        print("Expected error message: {0}\n"
-                            .format(expected_failures[c_name]))
+                if (expected_failures[c_name] is not None and
+                    expected_failures[c_name] not in str(exception)):
+                    bad += 1
+                    failed.append(c_name)
+                    print("Expected error message: {0}\n".format(expected_failures[c_name]))
                 else:
                     xfail += 1
                     print("OK-expected failure\n")
