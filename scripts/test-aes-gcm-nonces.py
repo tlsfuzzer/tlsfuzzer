724a1ca2937d7382ed7f89b4d27ed4f591c4c64f
Noam Rathaus <rathaus@gmail.com>
2023-05-27 08:46:25 +0300

    JSON output

--- scripts/test-aes-gcm-nonces.py
+++ scripts/test-aes-gcm-nonces.py
@@ -8,6 +8,7 @@ import sys
 import getopt
 from itertools import chain
 from random import sample
+import json
 
 from tlsfuzzer.runner import Runner
 from tlsfuzzer.messages import Connect, ClientHelloGenerator, \
@@ -43,6 +44,7 @@ def help_msg():
     print("                usage: [-x probe-name] [-X exception], order is compulsory!")
     print(" -n num         run 'num' or all(if 0) tests instead of default(all)")
     print("                (excluding \"sanity\" tests)")
+    print(" -j filename    JSON output into a filename")
     print(" --help         this message")
 
 
@@ -54,30 +56,47 @@ def main():
     run_exclude = set()
     expected_failures = {}
     last_exp_tmp = None
+    json_filename = None
 
     argv = sys.argv[1:]
-    opts, args = getopt.getopt(argv, "h:p:e:x:X:n:", ["help"])
+    opts, args = getopt.getopt(argv, "h:p:e:x:X:n:j:", ["help"])
     for opt, arg in opts:
+        if opt == '-j':
+            json_filename = arg
+            continue
+
         if opt == '-h':
             host = arg
-        elif opt == '-p':
+            continue
+
+        if opt == '-p':
             port = int(arg)
-        elif opt == '-e':
+            continue
+
+        if opt == '-e':
             run_exclude.add(arg)
-        elif opt == '-x':
+            continue
+
+        if opt == '-x':
             expected_failures[arg] = None
             last_exp_tmp = str(arg)
-        elif opt == '-X':
+            continue
+
+        if opt == '-X':
             if not last_exp_tmp:
                 raise ValueError("-x has to be specified before -X")
             expected_failures[last_exp_tmp] = str(arg)
-        elif opt == '-n':
+            continue
+
+        if opt == '-n':
             num_limit = int(arg)
-        elif opt == '--help':
+            continue
+
+        if opt == '--help':
             help_msg()
             sys.exit(0)
-        else:
-            raise ValueError("Unknown option: {0}".format(opt))
+        
+        raise ValueError("Unknown option: {0}".format(opt))
 
     if args:
         run_only = set(args)
@@ -159,6 +178,14 @@ def main():
 
     conversations["aes-256-gcm cipher"] = conversation
 
+    outcome = {
+        'good' : [],
+        'bad' : [],
+        'xfail' : [],
+        'xpass' : [],
+        'failed' : [],
+        'xpassed' : [],
+    }
 
     good = 0
     bad = 0
@@ -203,54 +230,68 @@ def main():
         if c_name in expected_failures:
             if res:
                 xpass += 1
+                outcome['xpassed'].append(c_name)
                 xpassed.append(c_name)
                 print("XPASS-expected failure but test passed\n")
             else:
                 if expected_failures[c_name] is not None and  \
                     expected_failures[c_name] not in str(exception):
                         bad += 1
+                        outcome['failed'].append(c_name)
                         failed.append(c_name)
                         print("Expected error message: {0}\n"
                             .format(expected_failures[c_name]))
                 else:
+                    outcome['xfail'].append(c_name)
                     xfail += 1
                     print("OK-expected failure\n")
         else:
             if res:
+                outcome['good'].append(c_name)
                 good += 1
                 print("OK\n")
             else:
+                outcome['bad'].append(c_name)
+                outcome['failed'].append(c_name)
                 bad += 1
                 failed.append(c_name)
 
     print("aes-128-gcm Nonce monotonicity...")
     if len(nonces) < 2:
         print("Not enough nonces collected, FAIL")
+        outcome['bad'].append(c_name)
         bad += 1
     else:
         if bytesToNumber(nonces[0]) == bytesToNumber(nonces[1]):
             print("reused nonce! Security vulnerability!")
+            outcome['bad'].append(c_name)
             bad += 1
         elif bytesToNumber(nonces[0]) + 1 != bytesToNumber(nonces[1]):
             print("nonce not monotonically increasing, FAIL")
+            outcome['bad'].append(c_name)
             bad += 1
         else:
             print("OK\n")
+            outcome['good'].append(c_name)
             good += 1
 
     print("aes-256-gcm Nonce monotonicity...")
     if len(nonces256) < 2:
         print("Not enough nonces collected, FAIL")
+        outcome['bad'].append(c_name)
         bad += 1
     else:
         if bytesToNumber(nonces256[0]) == bytesToNumber(nonces256[1]):
             print("reused nonce! Security vulnerability!")
+            outcome['bad'].append(c_name)
             bad += 1
         elif bytesToNumber(nonces256[0]) + 1 != bytesToNumber(nonces256[1]):
             print("nonce not monotonically increasing, FAIL")
+            outcome['bad'].append(c_name)
             bad += 1
         else:
             print("OK\n")
+            outcome['good'].append(c_name)
             good += 1
 
     print("Test end")
@@ -270,6 +311,10 @@ def main():
     sort = sorted(failed, key=natural_sort_keys)
     if len(sort):
         print("FAILED:\n\t{0}".format('\n\t'.join(repr(i) for i in sort)))
+
+    if json_filename is not None:
+        with open(json_filename, "w", encoding="utf-8") as file_handle:
+            file_handle.write(json.dumps(outcome, indent=1))
 
     if bad or xpass:
         sys.exit(1)
