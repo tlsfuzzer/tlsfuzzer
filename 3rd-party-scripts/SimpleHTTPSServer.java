1efe3d1073a7c6b97e988c7771b375f503703d87
Hubert Kario <hkario@redhat.com>
2023-12-28 16:28:21 +0100

    add a simple Java test server
    since Java doesn't have a simple test tool, for server to test
    against we need a test application, provide one to reference
    from the wiki

---
+++ 3rd-party-scripts/SimpleHTTPSServer.java
@@ -0,0 +1,105 @@
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.lang.*;
+import java.net.URL;
+import com.sun.net.httpserver.HttpsServer;
+import java.security.KeyStore;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.TrustManagerFactory;
+import com.sun.net.httpserver.*;
+import javax.net.ssl.SSLEngine;
+import javax.net.ssl.SSLParameters;
+
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URLConnection;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.security.cert.X509Certificate;
+
+import java.net.InetAddress;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpsExchange;
+
+public class SimpleHTTPSServer {
+
+    public static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            String response = "This is the response";
+            HttpsExchange httpsExchange = (HttpsExchange) t;
+            t.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
+            t.sendResponseHeaders(200, response.getBytes().length);
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) throws Exception {
+
+        try {
+            // setup the socket address
+            InetSocketAddress address = new InetSocketAddress(4433);
+
+            // initialise the HTTPS server
+            HttpsServer httpsServer = HttpsServer.create(address, 0);
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+
+            // initialise the keystore
+            char[] password = "password".toCharArray();
+            KeyStore ks = KeyStore.getInstance("JKS");
+            FileInputStream fis = new FileInputStream("testkey.jks");
+            ks.load(fis, password);
+
+            // setup the key manager factory
+            KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+            kmf.init(ks, password);
+
+            // setup the trust manager factory
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+            tmf.init(ks);
+
+            // setup the HTTPS context and parameters
+            sslContext.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
+            httpsServer.setHttpsConfigurator(new HttpsConfigurator(sslContext) {
+                public void configure(HttpsParameters params) {
+                    try {
+                        // initialise the SSL context
+                        SSLContext context = getSSLContext();
+                        SSLEngine engine = context.createSSLEngine();
+                        params.setNeedClientAuth(false);
+                        params.setCipherSuites(engine.getEnabledCipherSuites());
+                        params.setProtocols(engine.getEnabledProtocols());
+
+                        // Set the SSL parameters
+                        SSLParameters sslParameters = context.getSupportedSSLParameters();
+                        params.setSSLParameters(sslParameters);
+
+                    } catch (Exception ex) {
+                        System.out.println("Failed to create HTTPS port");
+                    }
+                }
+            });
+            httpsServer.createContext("/test", new MyHandler());
+            httpsServer.setExecutor(null); // creates a default executor
+            httpsServer.start();
+
+        } catch (Exception exception) {
+            System.out.println("Failed to create HTTPS server on port " + 8000 + " of localhost");
+            exception.printStackTrace();
+
+        }
+    }
+
+}
