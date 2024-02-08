e92e2908421e05a6badb26bb1caa2f2932db1a73
Hubert Kario <hkario@redhat.com>
2024-02-08 19:18:18 +0100

    Multi(threaded|processed) Skillings-Mack

--- tests/test_tlsfuzzer_utils_stats.py
+++ tests/test_tlsfuzzer_utils_stats.py
@@ -7,7 +7,9 @@ import random
 failed_import = False
 try:
     from tlsfuzzer.utils.stats import skillings_mack_result, \
-        skillings_mack_test
+        skillings_mack_test, _block_slices, _slices, _summarise_chunk, \
+        _set_unique
+    import tlsfuzzer.utils.stats
     import numpy as np
 except ImportError:
     failed_import = True
@@ -15,11 +17,160 @@ except ImportError:
 
 @unittest.skipIf(failed_import,
                  "Numpy missing")
-class TestSkillingsMackTest(unittest.TestCase):
+class TestSummariseChunk(unittest.TestCase):
     def assertEqualApprox(self, a, b, eta=1e-6):
-        if a > b * (1 + eta) or a < b * (1 - eta):
+        if abs(a - b) > eta:
             raise AssertionError("{0} is not approximately equal {1}"
                                  .format(a, b))
+
+    def test_summarise_chunk(self):
+        tlsfuzzer.utils.stats._values = \
+            [3, 5, 15, 1, 3, 18, 5, 4, 21, 2, 6, 0, 2, 17, 0, 2, 10, 0,
+             3, 8, 0, 2, 13]
+        tlsfuzzer.utils.stats._groups = \
+            ['1', '2', '3', '1', '2', '3', '1', '2', '3', '1', '3', '1',
+             '2', '3', '1', '2', '3', '1', '2', '3', '1', '2', '3']
+        all_groups = set(tlsfuzzer.utils.stats._groups)
+        len_groups = len(tlsfuzzer.utils.stats._groups)
+        tlsfuzzer.utils.stats._blocks = \
+            [1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7,
+             8, 8, 8]
+
+        ret = _summarise_chunk((all_groups, None, (0, len_groups)))
+
+        adjusted_ranks, block_counts, pair_counts = ret
+
+        self.assertEqual(len(adjusted_ranks), 3)
+        self.assertEqualApprox(adjusted_ranks['3'], 13.124355652982139)
+        self.assertEqualApprox(adjusted_ranks['1'], -11.392304845413262)
+        self.assertEqualApprox(adjusted_ranks['2'], -1.7320508075688772)
+        self.assertEqual(block_counts, {'2': 14, '1': 15, '3': 15})
+        self.assertEqual(pair_counts,
+            {frozenset(['2', '1']): 7, frozenset(['3', '1']): 8,
+             frozenset(['3', '2']): 7})
+
+    def test_summarise_chunk_duplicate_use_last(self):
+        # check if it produces the same values as the example from PMCMRplus
+        # R module documentation
+        tlsfuzzer.utils.stats._values = \
+                [3, 5, 15, 1, 3, 18, 5, 4, 21, 2, 6, 0, 2, 17, 0, 2, 10, 0,
+                  3, 8, 0, 2, 0, 13]
+        tlsfuzzer.utils.stats._groups = \
+                ['1', '2', '3', '1', '2', '3', '1', '2', '3', '1', '3', '1',
+                  '2', '3', '1', '2', '3', '1', '2', '3', '1', '2', '3', '3']
+        all_groups = set(tlsfuzzer.utils.stats._groups)
+        len_groups = len(tlsfuzzer.utils.stats._groups)
+        tlsfuzzer.utils.stats._blocks = \
+                [1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7,
+                  8, 8, 8, 8]
+
+        ret = _summarise_chunk((all_groups, 'last', (0, len_groups)))
+
+        adjusted_ranks, block_counts, pair_counts = ret
+
+        self.assertEqual(len(adjusted_ranks), 3)
+        self.assertEqualApprox(adjusted_ranks['3'], 13.124355652982139)
+        self.assertEqualApprox(adjusted_ranks['1'], -11.392304845413262)
+        self.assertEqualApprox(adjusted_ranks['2'], -1.7320508075688772)
+        self.assertEqual(block_counts, {'2': 14, '1': 15, '3': 15})
+        self.assertEqual(pair_counts,
+            {frozenset(['2', '1']): 7, frozenset(['3', '1']): 8,
+             frozenset(['3', '2']): 7})
+
+    def test_summarise_chunk_duplicates(self):
+        # check if it produces the same values as the example from PMCMRplus
+        # R module documentation
+        tlsfuzzer.utils.stats._values = \
+                [3, 5, 15, 1, 3, 18, 5, 4, 21, 2, 6, 0, 2, 17, 0, 2, 10, 0,
+                  3, 8, 0, 2, 0, 13]
+        tlsfuzzer.utils.stats._groups = \
+                ['1', '2', '3', '1', '2', '3', '1', '2', '3', '1', '3', '1',
+                  '2', '3', '1', '2', '3', '1', '2', '3', '1', '2', '3', '3']
+        all_groups = set(tlsfuzzer.utils.stats._groups)
+        len_groups = len(tlsfuzzer.utils.stats._groups)
+        tlsfuzzer.utils.stats._blocks = \
+                [1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7,
+                  8, 8, 8, 8]
+
+        with self.assertRaises(ValueError) as e:
+            ret = _summarise_chunk((all_groups, None, (0, len_groups)))
+
+        self.assertIn("Duplicate group (3) in block (8)", str(e.exception))
+
+    def test_summarise_chunk_duplicate_use_first(self):
+        # check if it produces the same values as the example from PMCMRplus
+        # R module documentation
+        tlsfuzzer.utils.stats._values = \
+                [3, 5, 15, 1, 3, 18, 5, 4, 21, 2, 6, 0, 2, 17, 0, 2, 10, 0,
+                  3, 8, 0, 2, 13, 0]
+        tlsfuzzer.utils.stats._groups = \
+                ['1', '2', '3', '1', '2', '3', '1', '2', '3', '1', '3', '1',
+                  '2', '3', '1', '2', '3', '1', '2', '3', '1', '2', '3', '3']
+        all_groups = set(tlsfuzzer.utils.stats._groups)
+        len_groups = len(tlsfuzzer.utils.stats._groups)
+        tlsfuzzer.utils.stats._blocks = \
+                [1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7,
+                  8, 8, 8, 8]
+
+        ret = _summarise_chunk((all_groups, 'first', (0, len_groups)))
+
+        adjusted_ranks, block_counts, pair_counts = ret
+
+        self.assertEqual(len(adjusted_ranks), 3)
+        self.assertEqualApprox(adjusted_ranks['3'], 13.124355652982139)
+        self.assertEqualApprox(adjusted_ranks['1'], -11.392304845413262)
+        self.assertEqualApprox(adjusted_ranks['2'], -1.7320508075688772)
+        self.assertEqual(block_counts, {'2': 14, '1': 15, '3': 15})
+        self.assertEqual(pair_counts,
+            {frozenset(['2', '1']): 7, frozenset(['3', '1']): 8,
+             frozenset(['3', '2']): 7})
+
+    def test_summarise_not_sorted(self):
+        tlsfuzzer.utils.stats._values = [10, 20, 30, 40, 50]
+        tlsfuzzer.utils.stats._groups = [0, 1, 2, 0, 2]
+        tlsfuzzer.utils.stats._blocks = [0, 0, 1, 1, 0]
+        all_groups = set(tlsfuzzer.utils.stats._groups)
+        len_groups = len(tlsfuzzer.utils.stats._groups)
+        with self.assertRaises(ValueError) as e:
+            ret = _summarise_chunk((all_groups, None, (0, len_groups)))
+
+        self.assertIn("blocks are not sorted", str(e.exception))
+
+    def test_summarise_empty(self):
+        tlsfuzzer.utils.stats._values = []
+        tlsfuzzer.utils.stats._groups = []
+        tlsfuzzer.utils.stats._blocks = []
+        all_groups = set(tlsfuzzer.utils.stats._groups)
+        len_groups = len(tlsfuzzer.utils.stats._groups)
+        with self.assertRaises(ValueError) as e:
+            ret = _summarise_chunk((all_groups, None, (0, len_groups)))
+
+        self.assertIn("Empty data set", str(e.exception))
+
+    def test_set_unique(self):
+        tlsfuzzer.utils.stats._groups = \
+            ['1', '2', '3', '1', '2', '3', '1', '2', '3', '1', '3', '1',
+             '2', '3', '1', '2', '3', '1', '2', '3', '1', '2', '3']
+
+        self.assertEqual(_set_unique((0, 24)), set(['1', '2', '3']))
+
+
+@unittest.skipIf(failed_import,
+                 "Numpy missing")
+class TestSkillingsMackTest(unittest.TestCase):
+    def assertEqualApprox(self, a, b, eta=1e-5):
+        if abs(a - b) > eta:
+            raise AssertionError("{0} is not approximately equal {1}"
+                                 .format(a, b))
+
+    def test_with_different_lengths(self):
+        vals = [0, 0]
+        groups = [0, 1, 2]
+        blocks = [0, 0, 0]
+        with self.assertRaises(ValueError) as e:
+            skillings_mack_test(vals, groups, blocks)
+
+        self.assertIn("must be the same length", str(e.exception))
 
     def test_with_duplcate_group_block_pairs(self):
         vals = [0, 0, 0]
@@ -163,3 +314,62 @@ class TestSkillingsMackTest(unittest.TestCase):
         res = skillings_mack_test(vals, groups, blocks)
 
         self.assertGreater(res.p_value, 1e-6)
+
+
+@unittest.skipIf(failed_import,
+                 "Numpy missing")
+class TestBlockSlices(unittest.TestCase):
+    def test_chunk_larger_than_data(self):
+        blocks = [0, 0, 1, 1]
+        ret = list(_block_slices(blocks, 10))
+        self.assertEqual(ret, [(0, 4)])
+
+    def test_slice_on_chunk_bounary(self):
+        blocks = [0, 0, 0, 1, 1, 1]
+        ret = list(_block_slices(blocks, 3))
+        self.assertEqual(ret, [(0, 3), (3, 6)])
+
+    def test_slice_on_chunk_bounary_multiple_chunks(self):
+        blocks = [0, 0, 1, 1, 2, 2, 3, 3]
+        ret = list(_block_slices(blocks, 2))
+        self.assertEqual(ret, [(0, 2), (2, 4), (4, 6), (6, 8)])
+
+    def test_slice_not_on_chunk_boundary(self):
+        blocks = [0, 0, 0, 1, 1, 1, 2, 2, 2]
+        ret = list(_block_slices(blocks, 4))
+        self.assertEqual(ret, [(0, 6), (6, 9)])
+
+    def test_one_slice(self):
+        blocks = [0, 0, 0, 0, 0, 0, 0, 0]
+        ret = list(_block_slices(blocks, 3))
+        self.assertEqual(ret, [(0, 8)])
+
+    def test_one_slice_at_end(self):
+        blocks = [0, 0, 0, 1, 1, 1, 1, 1, 1]
+        ret = list(_block_slices(blocks, 2))
+        self.assertEqual(ret, [(0, 3), (3, 9)])
+
+
+
+@unittest.skipIf(failed_import,
+                 "Numpy missing")
+class TestSlices(unittest.TestCase):
+    def test_empty(self):
+        ret = list(_slices(0, 10))
+        self.assertEqual(ret, [])
+
+    def test_smaller_than_chunk(self):
+        ret = list(_slices(4, 10))
+        self.assertEqual(ret, [(0, 4)])
+
+    def test_equal_to_chunk_size(self):
+        ret = list(_slices(10, 10))
+        self.assertEqual(ret, [(0, 10)])
+
+    def test_multiple_slices(self):
+        ret = list(_slices(10, 2))
+        self.assertEqual(ret, [(0, 2), (2, 4), (4, 6), (6, 8), (8, 10)])
+
+    def test_length_not_multiple_of_slice_length(self):
+        ret = list(_slices(10, 4))
+        self.assertEqual(ret, [(0, 4), (4, 8), (8, 10)])
--- tlsfuzzer/utils/stats.py
+++ tlsfuzzer/utils/stats.py
@@ -2,9 +2,11 @@
 # Released under Gnu GPL v2.0, see LICENCE for details
 """Various statistical methods missing from scipy and numpy."""
 
+import os
 from math import sqrt
 from itertools import combinations
 from collections import defaultdict, namedtuple
+from multiprocessing import Pool
 from scipy.stats import rankdata, distributions
 import numpy as np
 
@@ -33,42 +35,57 @@ def _summarise_tuple(current_block, all_groups, adjusted_ranks, block_counts,
         else:
             block_counts[g] += (len_current_block - 1)
         adjusted_ranks[g] += sqrt(12/(len_current_block + 1)) * \
-                             (ranks[g] - (len_current_block + 1) / 2.)
+            (ranks[g] - (len_current_block + 1) / 2.)
 
 
-def skillings_mack_test(values, groups, blocks, duplicates=None):
-    """Perform the Skillings-Mack rank sum test.
+def _set_unique(idx):
+    global _groups
+    return set(np.unique(_groups[idx[0]:idx[1]]))
 
-    Skillings-Mack test is a Friedman-like test for unbalanced incomplete
-    block design data. The null hypothesis is that no group stochastically
-    dominates any other group, alternative hypothesis is that in at least
-    one pair of groups one stochastically dominates the other.
 
-    The test requires measurements to be independent within blocks and
-    that the missing values are either partially balanced or random.
+def _slices(length, chunksize):
+    """Return an iterator with tuples that describe the start and end of
+    array of ``length`` where each slice has at most ``chunksize`` elements"""
+    if length == 0:
+        return
+    starts = iter(range(0, length, chunksize))
+    start = next(starts)
+    for i in starts:
+        yield (start, i)
+        start = i
+    yield (start, length)
 
-    ``values``, ``groups``, and ``blocks`` must have equal length.
 
-    Reference: Skillings, J. H., Mack, G.A. (1981) On the use of a
-    Friedman-type statistic in balanced and unbalanced block designs,
-    Technometrics 23, 171-177
+def _block_slices(blocks, chunksize):
+    """Return an iterator with tuples that describe the start and end of
+    array of sorted ids in ``blocks`` where the chunk breaks happen on
+    id change and every chunk is approximately ``chunksize`` elements in
+    size."""
+    start = 0
+    while True:
+        if len(blocks) - start <= chunksize:
+            yield (start, len(blocks))
+            break
+        start_block = blocks[start+chunksize-1]
+        for i, block in enumerate(blocks[start+chunksize:], start + chunksize):
+            if block != start_block:
+                yield (start, i)
+                start = i
+                break
+        else:
+            yield (start, i+1)
+            break
 
-    :param iterable values: an iterable containing values that can be ranked
-      (int, float, etc.)
-    :param list groups: an iterable describing which group the corresponding
-      value belongs to, the elements must be hashable
-    :param iterable blocks: an iterable describing which test block the
-      corresponding value belongs to, the elements must be sorted in the
-      smallest-first order (e.g.: 1, 1, 1, 2, 2, 2, 2, 3,...)
-    :param str duplicates: if set to None (default), will refuse to process
-      duplicate data entries, if set to ``first`` it will use the first
-      value of specific group in the given block, if set to ``last`` it
-      will use the last value in a block
-    :return: named tuple with values of (``p_value``, ``T``, ``df``)
-    """
-    assert duplicates is None or duplicates in ('first', 'last')
 
-    all_groups = set(np.unique(groups))
+def _summarise_chunk(args):
+    global _groups
+    groups = _groups
+    global _values
+    values = _values
+    global _blocks
+    blocks = _blocks
+    all_groups, duplicates, idx = args
+    start, stop = idx
 
     current_block = None
     current_block_id = None
@@ -80,7 +97,9 @@ def skillings_mack_test(values, groups, blocks, duplicates=None):
     # how many times the values are paired with each-other
     pair_counts = defaultdict(int)
 
-    for val, group, block in zip(values, groups, blocks):
+    for val, group, block in zip(values[start:stop],
+                                 groups[start:stop],
+                                 blocks[start:stop]):
         # new block detected, summarise the current block, start a new one
         if block != current_block_id:
             # summarise the tuple only if there is more than one measurement
@@ -111,6 +130,89 @@ def skillings_mack_test(values, groups, blocks, duplicates=None):
     if len(current_block) > 1:
         _summarise_tuple(current_block, all_groups, adjusted_ranks,
                          block_counts, pair_counts)
+
+    return adjusted_ranks, block_counts, pair_counts
+
+
+def skillings_mack_test(values, groups, blocks, duplicates=None):
+    """Perform the Skillings-Mack rank sum test.
+
+    Skillings-Mack test is a Friedman-like test for unbalanced incomplete
+    block design data. The null hypothesis is that no group stochastically
+    dominates any other group, alternative hypothesis is that in at least
+    one pair of groups one stochastically dominates the other.
+
+    The test requires measurements to be independent within blocks and
+    that the missing values are either partially balanced or random.
+
+    ``values``, ``groups``, and ``blocks`` must have equal length.
+
+    Reference: Skillings, J. H., Mack, G.A. (1981) On the use of a
+    Friedman-type statistic in balanced and unbalanced block designs,
+    Technometrics 23, 171-177
+
+    Note: while the arguments have to be list-like, the function accepts
+    memory mapped files and allows processing data sets larger than
+    available system memory.
+
+    Function is not thread-safe.
+
+    :param list-like values: a list containing values that can be ranked
+      (int, float, etc.)
+    :param list-like groups: a list describing which group the corresponding
+      value belongs to, the elements must be hashable
+    :param list-like blocks: a list describing which test block the
+      corresponding value belongs to, the elements must be sorted in the
+      smallest-first order (e.g.: 1, 1, 1, 2, 2, 2, 2, 3,...)
+    :param str duplicates: if set to None (default), will refuse to process
+      duplicate data entries, if set to ``first`` it will use the first
+      value of specific group in the given block, if set to ``last`` it
+      will use the last value in a block
+    :return: named tuple with values of (``p_value``, ``T``, ``df``)
+    """
+    assert duplicates is None or duplicates in ('first', 'last')
+
+    if not (len(groups) == len(values) == len(blocks)):
+        raise ValueError("values, groups, and blocks must be the same length")
+
+    global _groups
+    _groups = groups
+    global _values
+    _values = values
+    global _blocks
+    _blocks = blocks
+
+    all_groups = set()
+    with Pool() as p:
+        # slice the data so that every worker has at least good few
+        # dozen megabytes of data to work with
+        chunks = p.imap_unordered(_set_unique, _slices(len(groups),
+                                                       1024*1024*64))
+
+        for i in chunks:
+            all_groups.update(i)
+
+        adjusted_ranks = defaultdict(float)
+        # how many times a group is present in a block adjusted by individual
+        # block sizes
+        block_counts = defaultdict(int)
+        # how many times the values are paired with each-other
+        pair_counts = defaultdict(int)
+
+        chunk_size = min(1024*1024*64,
+                         max(10, len(blocks) // os.cpu_count()))
+
+        chunks = p.imap_unordered(_summarise_chunk,
+                                  ((all_groups, duplicates, i) for i in
+                                   _block_slices(blocks, chunk_size)))
+
+        for chunk_ranks, chunk_block_counts, chunk_pair_counts in chunks:
+            for k, v in chunk_ranks.items():
+                adjusted_ranks[k] += v
+            for k, v in chunk_block_counts.items():
+                block_counts[k] += v
+            for k, v in chunk_pair_counts.items():
+                pair_counts[k] += v
 
     # check if all the groups present in data were compared with at
     # least one other group
