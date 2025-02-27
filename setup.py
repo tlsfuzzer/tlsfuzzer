e66d16d8f4b1d3c830eae27f857ba8af7d7b0849
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2025-02-27 17:59:03 +0100

    📦 Declare groups of optional deps @ pkg meta
    Ref #903. Follow-up to #991.

--- setup.py
+++ setup.py
@@ -16,4 +16,32 @@ setup(name="tlsfuzzer",
       description="TLS test suite and fuzzer.",
       license="GPLv2",
       install_requires=["ecdsa >= 0.15", "tlslite-ng >= 0.8.2"],
+      extras_require={
+          "analysis": [
+              # Additionally to `tlsfuzzer.analysis`, this also satisfies the
+              # following apps:
+              # test_bleichenbacher_timing_marvin
+              # test_lucky13
+              # test_tls13_minerva
+              # test_bleichenbacher_timing_pregenerate
+              "matplotlib",
+              "numpy",
+              "pandas",
+              "scipy",
+          ],
+          "execution": [
+              "zstd",  # test_tls13_client_certificate_compression
+          ],
+          "extraction": [
+              # Additionally to `tlsfuzzer.analysis`, this partially satisfies
+              # the following apps:
+              # test_bleichenbacher_timing_marvin
+              # test_lucky13
+              # test_tls13_minerva
+              # test_bleichenbacher_timing_pregenerate
+              "dpkt",
+              "numpy",
+              "pandas",
+          ],
+      },
       packages=["tlsfuzzer", "tlsfuzzer.utils"])
